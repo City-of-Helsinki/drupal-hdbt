78e5be010c61dc3b9c720fb3d27ba6b44ccc6be8
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-11-04 22:52:07 +0200

    Added linters for scss and js.

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 (($, Drupal, drupalSettings) => {
   Drupal.behaviors.themeCommon = {
     attach: function attach() {
       // Code here.
     }
   };
+// eslint-disable-next-line no-undef
 })(jQuery, Drupal, drupalSettings);
