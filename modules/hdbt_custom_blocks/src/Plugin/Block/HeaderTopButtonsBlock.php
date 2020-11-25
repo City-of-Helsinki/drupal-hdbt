bb022806aa87ec2639fdd8a7b146c70e05c6c438
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-11-25 13:03:18 +0200

    Added HDBT Custom blocks module to support custom blocks in HDBT.

---
+++ modules/hdbt_custom_blocks/src/Plugin/Block/HeaderTopButtonsBlock.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\hdbt_custom_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'HeaderTopButtonsBlock' block.
+ *
+ * @Block(
+ *  id = "header_top_buttons_block",
+ *  admin_label = @Translation("Header Top buttons block"),
+ * )
+ */
+class HeaderTopButtonsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $build['#button_title'] = t('Placeholder');
+    $build['#theme'] = 'header_top_button';
+
+    return $build;
+  }
+
+}
