a61e2caa048f6c09cd271f3faa20738072119db4
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-12-08 11:48:11 +0200

    Added support for admin tools site settings. Koro, footer color and footer content is presented via configuration settings.

---
+++ modules/hdbt_custom_blocks/src/Plugin/Block/FooterTopBlock.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\hdbt_custom_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides a 'FooterTopBlock' block.
+ *
+ * @Block(
+ *  id = "footer_top_block",
+ *  admin_label = @Translation("Footer top block"),
+ * )
+ */
+class FooterTopBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $settings = \Drupal::config('hdbt_admin_tools.site_settings');
+
+    $build['#theme'] = 'footer_top_content';
+    $build['#title'] = $settings->get('footer_settings.footer_top_title');
+    $build['#content'] = [
+      '#type' => 'processed_text',
+      '#text' => $settings->get('footer_settings.footer_top_content.value'),
+      '#format' => $settings->get('footer_settings.footer_top_content.format'),
+    ];
+
+    return $build;
+  }
+
+}
