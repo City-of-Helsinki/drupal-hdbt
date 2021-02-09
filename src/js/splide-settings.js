bd7f7c4ed7c0627411c1ef9ad49790d3893dd5ab
Tero Elonen <tero.elonen@druid.fi>
2021-02-09 13:46:11 +0200

    UHF-201 Implement thumbnails navigation for gallery, style it a bit

--- src/js/splide-settings.js
+++ src/js/splide-settings.js
@@ -1,4 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
   /* global Splide */
-  new Splide('.splide').mount();
+  const splide = new Splide('.splide', {
+    pagination: false,
+  });
+
+  // Collect elements of thumbnails.
+  const images = document.querySelectorAll(
+    '.js-gallery__thumbnails .gallery__thumbnails__item'
+  );
+
+  let activeImage;
+  const activeClass = 'is-active';
+
+  // Listen a click event and toggle a class.
+  for (let i = 0, len = images.length; i < len; i++) {
+    let image = images[i];
+
+    splide.on(
+      'click',
+      function () {
+        if (activeImage !== image) {
+          activeImage.classList.remove(activeClass);
+          image.classList.add(activeClass);
+          splide.go(i);
+          activeImage = image;
+        }
+      },
+      image
+    );
+  }
+
+  splide.on('mounted move', function (newIndex) {
+    // newIndex will be undefined through the "mounted" event.
+    const image = images[newIndex !== undefined ? newIndex : splide.index];
+
+    if (image && activeImage !== image) {
+      if (activeImage) {
+        activeImage.classList.remove(activeClass);
+      }
+
+      image.classList.add(activeClass);
+      activeImage = image;
+    }
+  });
+
+  splide.mount();
 });
