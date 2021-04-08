704b1f2b01bc4ce11c4262a70d3226786466ba5d
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-04-08 16:30:49 +0300

    UHF-1090: UHF-1090: Fixed coding standards.

--- modules/hdbt_content/src/Plugin/Block/FooterTopBlock.php
+++ modules/hdbt_content/src/Plugin/Block/FooterTopBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\hdbt_content\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\Config;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'FooterTopBlock' block.
@@ -12,21 +15,56 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Footer top block"),
  * )
  */
-class FooterTopBlock extends BlockBase {
+class FooterTopBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Contains the hdbt_admin_tools.site_settings configuration object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected Config $adminToolsConfig;
+
+  /**
+   * Constructs a new FooterTopBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\Config $admin_tools_config
+   *   Admin tools configuration.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Config $admin_tools_config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->adminToolsConfig = $admin_tools_config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')->get('hdbt_admin_tools.site_settings')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $settings = \Drupal::config('hdbt_admin_tools.site_settings');
 
     $build['#theme'] = 'footer_top_content';
-    $build['#title'] = $settings->get('footer_settings.footer_top_title');
+    $build['#title'] = $this->adminToolsConfig->get('footer_settings.footer_top_title');
     $build['#content'] = [
       '#type' => 'processed_text',
-      '#text' => $settings->get('footer_settings.footer_top_content.value'),
-      '#format' => $settings->get('footer_settings.footer_top_content.format'),
+      '#text' => $this->adminToolsConfig->get('footer_settings.footer_top_content.value'),
+      '#format' => $this->adminToolsConfig->get('footer_settings.footer_top_content.format'),
     ];
 
     return $build;
