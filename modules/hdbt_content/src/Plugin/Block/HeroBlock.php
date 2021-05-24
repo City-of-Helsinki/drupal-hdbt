a1d5caa440c54e36e59f34a201ad7713a766b7c6
Tero Elonen <tero.elonen@druid.fi>
2021-05-24 19:07:06 +0300

    UHF-1280: Fix issues with hero block and title being displayed at the same time, style the link back to the previous level on menu

--- modules/hdbt_content/src/Plugin/Block/HeroBlock.php
+++ modules/hdbt_content/src/Plugin/Block/HeroBlock.php
@@ -21,13 +21,29 @@ class HeroBlock extends BlockBase {
     $node = \Drupal::routeMatch()->getParameter('node');
     $build = [];
 
-    if (!empty($node)) {
-      $paragraph_id = $node->field_hero->target_id;
+    if (!empty($node) && $node->hasField('field_has_hero')) {
+      $paragraph_id = FALSE;
+      $first_paragraph_grey = '';
+
+      if (boolval($node->get('field_has_hero')->getString())) {
+        $paragraph_id = $node->field_hero->target_id;
+      }
+
+      // Handle only landing page.
+      if ($node->getType() === 'landing_page') {
+        // Check if the content field first paragraph is Unit search
+        // and add classes accordingly.
+        $paragraph = $node->get('field_content')->first()->entity;
+        if (!empty($paragraph) && $paragraph->getType() === 'unit_search') {
+          $first_paragraph_grey = 'has-first-gray-bg-block';
+        }
+      }
 
       $build = [
         '#theme' => 'hero_block',
         '#title' => $this->t('Hero block'),
         '#paragraph_id' => $paragraph_id,
+        '#first_paragraph_grey' => $first_paragraph_grey,
         '#cache' => [
           'tags' => [
             'node:' . $node->id,
