f4ffc95163540e0e7a0593b1d3c162f99b17718e
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-06-08 08:17:43 +0300

    UHF-1507: Themed mobile menu.

--- src/js/mmenu-config.js
+++ src/js/mmenu-config.js
@@ -1,43 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Get the burger link element.
+  const burger = document.getElementById('menu-hamburger');
+  const languageSwitcher = document.getElementById('block-language-switcher');
+
+  // Fire the mmenu plugin.
   /* global Mmenu */
   const menu = new Mmenu('#mobile-navigation', {
+    slidingSubmenus: false,
     lazySubmenus: true,
-    extensions: ['pagedim-black'],
-    // columns: true,
+    extensions: ['position-bottom', 'fullscreen'],
     setSelected: {
-      current: true,
+      hover: true,
+      parent: true,
+    },
+    keyboardNavigation: {
+      enable: true,
     },
     navbars: [
       {
-        position: 'top',
-        content: ['searchfield'],
-      },
-      {
-        position: 'top',
-        content: ['prev', 'title'],
-      },
-      {
         position: 'bottom',
         content: [
-          '<a class="fa fa-envelope" href="#">Paikka a</a>',
-          '<a class="fa fa-twitter" href="#">Juttu b</a>',
-          '<a class="fa fa-facebook" href="#">Jotain c</a>',
+          '<div id="mobile-navigation-footer" class="mobile-navigation__footer"></div>',
         ],
       },
     ],
   });
 
-  const burger = document.getElementById('menu-hamburger');
-
-  burger.addEventListener('click', function () {
-    let parent = menu.querySelector('.is-active');
-    parent.parentNode.querySelector('.mm-btn_next').trigger('click');
-  });
-
-  // Get the API
+  // Get the mmenu API
   const api = menu.API;
 
-  // Invoke a method
-  const panel = document.querySelector('#mobile-navigation .is-active');
-  api.openPanel(panel.parentNode.parentNode);
+  // Bind the burger icon change to menu opening via API.
+  api.bind('open:start', () => {
+    burger.setAttribute('aria-expanded', 'true');
+    languageSwitcher.classList.remove('scroll-up');
+  });
+
+  // Bind the burger icon change to menu closing via API.
+  api.bind('close:start', () => {
+    burger.setAttribute('aria-expanded', 'false');
+  });
+
+  // Adter menu has initialized, copy contents.
+  api.bind('initMenu:after', () => {
+    // const headerTop = document.getElementById('hdbt-header-top');
+  });
+
+  // Get the links from the universal header.
+  // const headerTop = document.getElementById('hdbt-header-top');
+  //
+  // if (headerTop) {
+  //   Mobile footer elements.
+  // let mobileFooter = document.getElementById('mobile-navigation-footer');
+  // mobileFooter = headerTop.contents().clone();
+  // }
 });
