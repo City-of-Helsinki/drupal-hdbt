00618cb53d4b9e04da8593d1a5b72d773c254ffb
Lauri Kolehmainen <lauri.kolehmainen@siili.com>
2021-10-25 22:00:34 +0300

    UHF-2828: matomo & siteimprove id's from env variables

--- src/js/matomo.js
+++ src/js/matomo.js
@@ -1,11 +1,16 @@
-var _paq = window._paq = window._paq || [];
+var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function() {
-  var u='//webanalytics.digiaiiris.com/js/';
-  _paq.push(['setTrackerUrl', u+'tracker.php']);
-  _paq.push(['setSiteId', '141']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.src=u+'piwik.min.js'; s.parentNode.insertBefore(g,s);
+(function () {
+  var u = '//webanalytics.digiaiiris.com/js/';
+  _paq.push(['setTrackerUrl', u + 'tracker.php']);
+  _paq.push(['setSiteId', drupalSettings.matomo_site_id]);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript';
+  g.async = true;
+  g.src = u + 'piwik.min.js';
+  s.parentNode.insertBefore(g, s);
 })();
