f84b24bc9e7411e1c04550cb37b40d92cb442236
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2022-02-03 12:41:56 +0200

    UHF-2777: Fix broken env variable in webpack, add font token debugging information to CSS when in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const isDev = (process.env.NODE_ENV !== 'production');
-
 const path = require('path');
 const glob = require('glob');
 
@@ -8,6 +6,7 @@ const FriendlyErrorsWebpackPlugin = require('@nuxt/friendly-errors-webpack-plugi
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 const SvgToSprite = require('./webpack.svgToSprite');
+const { merge } = require('webpack-merge');
 
 // Handle entry points.
 const Entries = () => {
@@ -37,141 +36,191 @@ const Entries = () => {
   return entries;
 };
 
-module.exports = {
-  entry() {
-    return Entries();
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    chunkFilename: 'js/async/[name].chunk.js',
-    pathinfo: true,
-    filename: 'js/[name].min.js',
-    publicPath: '../',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        include: [
-          path.resolve(__dirname, 'src/icons')
-        ],
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|ttf|eot|svg)$/,
-        include: [
-          path.resolve(__dirname, 'src/fonts')
-        ],
-        generator: {
-          filename: 'fonts/[name][ext]'
-        },
-        type: 'asset/resource',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-        type: 'javascript/auto',
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: isDev,
-              importLoaders: 2,
-              esModule: false,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              'postcssOptions': {
-                'config': path.join(__dirname, 'postcss.config.js'),
-              },
-              sourceMap: isDev,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDev,
-            },
-          },
-        ],
-        type: 'javascript/auto',
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-    extensions: ['.js', '.json'],
-  },
-  plugins: [
-    new SvgToSprite(
-      path.resolve(__dirname, 'src/icons/**/*.svg'),
-      'icons/sprite.svg',
-      'icons.json'
-    ),
-    new FriendlyErrorsWebpackPlugin(),
-    new RemoveEmptyScriptsPlugin(),
-    new CopyPlugin({
-      'patterns': [
+
+module.exports = (env, argv) => {
+
+  const isDev = (argv.mode === 'development');
+
+  // Set the base config
+  const config = {
+    entry() {
+      return Entries();
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: 'js/async/[name].chunk.js',
+      pathinfo: true,
+      filename: 'js/[name].min.js',
+      publicPath: '../',
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          'from': 'node_modules/@splidejs/splide/dist/js/splide.min.js',
-          'to': path.resolve(__dirname, 'dist') + '/js/splide/',
-          'force': true,
-        }, {
-          'from': 'node_modules/@splidejs/splide/dist/css/splide-core.min.css',
-          'to': path.resolve(__dirname, 'dist') + '/css/splide/',
-          'force': true,
-        }, {
-          'from': 'node_modules/tiny-slider/dist/min/tiny-slider.js',
-          'to': path.resolve(__dirname, 'dist') + '/js/tiny-slider/',
-          'force': true,
-        }, {
-          'from': 'node_modules/tiny-slider/dist/tiny-slider.css',
-          'to': path.resolve(__dirname, 'dist') + '/css/tiny-slider/',
-          'force': true,
-        }, {
-          'from': 'node_modules/handorgel/lib/js/umd/handorgel.min.js',
-          'to': path.resolve(__dirname, 'dist') + '/js/handorgel/',
-          'force': true,
-        }, {
-          'from': 'node_modules/handorgel/lib/css/handorgel.min.css',
-          'to': path.resolve(__dirname, 'dist') + '/css/handorgel/',
-          'force': true,
-        }, {
-          'from': 'node_modules/mmenu-js/dist/mmenu.js',
-          'to': path.resolve(__dirname, 'dist') + '/js/mmenu/',
-          'force': true,
-        }, {
-          'from': 'node_modules/mmenu-js/dist/mmenu.css',
-          'to': path.resolve(__dirname, 'dist') + '/css/mmenu/',
-          'force': true,
-        }
+          test: /\.svg$/,
+          include: [
+            path.resolve(__dirname, 'src/icons')
+          ],
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|ttf|eot|svg)$/,
+          include: [
+            path.resolve(__dirname, 'src/fonts')
+          ],
+          generator: {
+            filename: 'fonts/[name][ext]'
+          },
+          type: 'asset/resource',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+          type: 'javascript/auto',
+        },
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: isDev,
+                importLoaders: 2,
+                esModule: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                'postcssOptions': {
+                  'config': path.join(__dirname, 'postcss.config.js'),
+                },
+                sourceMap: isDev,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDev,
+                additionalData: "$debug_mode: " + isDev + ";",
+              },
+            },
+          ],
+          type: 'javascript/auto',
+        },
+      ],
+    },
+    resolve: {
+      modules: [
+        path.join(__dirname, 'node_modules'),
+      ],
+      extensions: ['.js', '.json'],
+    },
+    plugins: [
+      new SvgToSprite(
+        path.resolve(__dirname, 'src/icons/**/*.svg'),
+        'icons/sprite.svg',
+        'icons.json'
+      ),
+      new FriendlyErrorsWebpackPlugin(),
+      new RemoveEmptyScriptsPlugin(),
+      new CopyPlugin({
+        'patterns': [
+          {
+            'from': 'node_modules/@splidejs/splide/dist/js/splide.min.js',
+            'to': path.resolve(__dirname, 'dist') + '/js/splide/',
+            'force': true,
+          }, {
+            'from': 'node_modules/@splidejs/splide/dist/css/splide-core.min.css',
+            'to': path.resolve(__dirname, 'dist') + '/css/splide/',
+            'force': true,
+          }, {
+            'from': 'node_modules/tiny-slider/dist/min/tiny-slider.js',
+            'to': path.resolve(__dirname, 'dist') + '/js/tiny-slider/',
+            'force': true,
+          }, {
+            'from': 'node_modules/tiny-slider/dist/tiny-slider.css',
+            'to': path.resolve(__dirname, 'dist') + '/css/tiny-slider/',
+            'force': true,
+          }, {
+            'from': 'node_modules/handorgel/lib/js/umd/handorgel.min.js',
+            'to': path.resolve(__dirname, 'dist') + '/js/handorgel/',
+            'force': true,
+          }, {
+            'from': 'node_modules/handorgel/lib/css/handorgel.min.css',
+            'to': path.resolve(__dirname, 'dist') + '/css/handorgel/',
+            'force': true,
+          }, {
+            'from': 'node_modules/mmenu-js/dist/mmenu.js',
+            'to': path.resolve(__dirname, 'dist') + '/js/mmenu/',
+            'force': true,
+          }, {
+            'from': 'node_modules/mmenu-js/dist/mmenu.css',
+            'to': path.resolve(__dirname, 'dist') + '/css/mmenu/',
+            'force': true,
+          }
+        ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].min.css',
+      })
+    ],
+    watchOptions: {
+      aggregateTimeout: 300,
+    },
+    // Tell us only about the errors.
+    stats: 'errors-only',
+    // Suppress performance errors.
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    }
+  };
+
+  if (argv.mode === 'production') {
+    const TerserPlugin = require('terser-webpack-plugin');
+
+    const full_config = merge(config, {
+      mode: 'production',
+      devtool: false,
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: 2015,
+              format: {
+                comments: false,
+              },
+            },
+            extractComments: false,
+          }),
+        ],
+      },
+    });
+
+    return full_config;
+
+  } else if (argv.mode === 'development') {
+    const SourceMapDevToolPlugin = require('webpack/lib/SourceMapDevToolPlugin');
+
+    const full_config = merge(config, {
+      mode: 'development',
+      devtool: 'eval-source-map',
+      plugins: [
+        new SourceMapDevToolPlugin({
+          filename: '[file].map',
+          exclude: [/node_modules/, /images/, /spritemap/, /svg-sprites/],
+        })
       ]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].min.css',
-    })
-  ],
-  watchOptions: {
-    aggregateTimeout: 300,
-  },
-  // Tell us only about the errors.
-  stats: 'errors-only',
-  // Suppress performance errors.
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+    });
+
+    return full_config;
+
   }
 };
