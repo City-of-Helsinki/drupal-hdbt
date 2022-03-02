8583fb5f2d5d92eb08c17ea15e7a21b7aa7395e4
Tero Elonen <tero.elonen@druid.fi>
2022-03-02 17:44:49 +0200

    UHF-4455: UHF-3454 Add ajax support for the tabs javascript functionality

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
+(function ($, drupalSettings) {
+  'use strict';
 
+  // Helper function to hide all tabbed content.
   function hideEverything(tabbedContent) {
     let allTabs = tabbedContent.querySelectorAll('.tab');
     let allContent = tabbedContent.querySelectorAll('.tab__content');
@@ -11,47 +13,87 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  // Make the given tab visible and hide other tabs.
   function toggleTabs(tab) {
     let tabParent = tab.closest('[data-drupal-selector="tabbed-content"]');
     let tabsContentId = tab.getAttribute('aria-controls');
     let tabsContent = document.getElementById(tabsContentId);
 
+    // First hide all tabs.
     hideEverything(tabParent);
 
+    // Then show the selected tab.
     tab.setAttribute('aria-selected', 'true');
     tabsContent.setAttribute('aria-hidden', 'false');
   }
 
-  const containers = document.querySelectorAll('[data-drupal-selector="tabbed-content"]');
+  // Save the active tab and its content to session storage.
+  function addToActiveTabStorage(activeTab) {
+    const tabId = activeTab.id;
+    const contentId = activeTab.getAttribute('aria-controls');
 
-  // Loop through tabbed content containers.
-  for (let i = 0; i < containers.length; i++) {
-    let instance = containers[i];
-    // Get the ID of the instance to be used to target elements.
-    const tabInstaceId = instance.dataset.idNumber;
-
-    // Get the first tab and its content as active tabs.
-    let activeTab = document.getElementById('tab-1--' + tabInstaceId);
-    let activeContent = document.getElementById('tab-1__content--' + tabInstaceId);
-
-    // Set them active with aria-attributes.
-    activeTab.setAttribute('aria-selected', 'true');
-    activeContent.setAttribute('aria-hidden', 'false');
-
-    const allTabs =  instance.querySelectorAll('.tab');
-
-    for (let i = 0; i < allTabs.length; i++) {
-      let tab = allTabs[i];
-
-      tab.addEventListener('click', function(event) {
-        // Toggle function.
-        toggleTabs(this);
-      });
-      tab.addEventListener('keydown', function(event) {
-        if (event.which === 13) {
-          toggleTabs(this);
-        }
-      });
+    if (tabId && contentId) {
+      window.sessionStorage.setItem('activeTab', tabId);
+      window.sessionStorage.setItem('activeContent', contentId);
     }
   }
-});
+
+  function initiateTabs(activeTab, activeContent) {
+    const containers = document.querySelectorAll('[data-drupal-selector="tabbed-content"]');
+
+    // Loop through tabbed content containers.
+    for (let i = 0; i < containers.length; i++) {
+      let instance = containers[i];
+      // Get the ID of the instance to be used to target elements.
+      const tabInstaceId = instance.dataset.idNumber;
+
+      // If the active tab is not set, use first tab as default.
+      if (activeTab == null) {
+        activeTab = 'tab-1--' + tabInstaceId;
+        activeContent = 'tab-1__content--' + tabInstaceId;
+      }
+
+      // Find the active tab elements.
+      let activeTabElement = document.getElementById(activeTab);
+      let activeContentElement = document.getElementById(activeContent);
+
+      // Set them active with aria-attributes.
+      activeTabElement.setAttribute('aria-selected', 'true');
+      activeContentElement.setAttribute('aria-hidden', 'false');
+
+      const allTabs = instance.querySelectorAll('.tab');
+
+      // Go through all tabs and add a listener for mouse click or keyboard enter.
+      for (let i = 0; i < allTabs.length; i++) {
+        let tab = allTabs[i];
+
+        tab.addEventListener('click', function () {
+          // Toggle function.
+          toggleTabs(this);
+          addToActiveTabStorage(this);
+        });
+        tab.addEventListener('keydown', function (event) {
+          if (event.which === 13) {
+            toggleTabs(this);
+            addToActiveTabStorage(this);
+          }
+        });
+      }
+    }
+  }
+
+  // Run after each ajax submit on the element that has tabs.
+  $(document).ajaxComplete(function(e, xhr, settings) {
+    if (settings.extraData.view_name === drupalSettings.tabsParent) {
+      let activeTab = window.sessionStorage.getItem('activeTab');
+      let activeContent =  window.sessionStorage.getItem('activeContent');
+      initiateTabs(activeTab, activeContent);
+    }
+  });
+
+  // Run after page is ready.
+  $(document).ready(function () {
+    initiateTabs();
+  });
+
+})(jQuery, drupalSettings);
