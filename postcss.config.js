779f07f2e186b5777ba78922836fa68501338ece
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2022-04-05 17:59:35 +0300

    Removed postcss-extend and converted postcss-strip-inline-comments to custom plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
-const isDev = process.env.NODE_ENV !== "production";
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  parser: "postcss-scss",
+  parser: 'postcss-scss',
   plugins: [
     // Plugins for PostCSS
-    ["autoprefixer", { sourceMap: isDev }], // Parses CSS and adds vendor prefixes.
-    "postcss-preset-env", // Convert modern CSS into something most browsers can understand.
-    "postcss-extend", // Minimize the number of repeat selectors and rules you write in CSS.
-    "postcss-nested", // Unwrap nested rules like how Sass does it.
-    "postcss-nesting", // Nest style rules inside each other, following the CSS Nesting specification.
-    "postcss-strip-inline-comments", // Strip inline comments.
+    ['autoprefixer', { sourceMap: isDev }], // Parses CSS and adds vendor prefixes.
+    'postcss-preset-env', // Convert modern CSS into something most browsers can understand.
+    'postcss-nested', // Unwrap nested rules like how Sass does it.
+    'postcss-nesting', // Nest style rules inside each other, following the CSS Nesting specification.
+    require('./postcss.plugins'), // Strip inline comments.
   ],
 };
