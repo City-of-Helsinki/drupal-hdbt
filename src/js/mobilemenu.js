7e9ed8d3f47bac1805da71a749337e1936e31baf
Sakari Laaksonen <ext.sakari.laaksonen@hel.fi>
2022-06-27 13:37:53 +0300

    UHF-6017: add loader spinner proto from hds-core, add slight scaling to panel animations so that scrolling does not yank when it occurs

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -2,7 +2,7 @@
 const Mustache = require('mustache');
 const mockmenu = require('./MOCK_MENU');
 const cls = require('classnames');
-// const once = require('lodash/once')
+const once = require('lodash/once');
 // view helper for showing button if current menu item has submenu items
 const button = function(){return this.items?.length>0; };
 /**
@@ -29,6 +29,15 @@ const Panel = {
         </div>
       </section>
     {{/panels}}
+    {{^panels}}
+    <div class="jsmenu__loading">
+    <div class="hds-loading-spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+    </div>
+    {{/panels}}
     `,
     listTemplate:`
     <ul class="jsmenu__items">
@@ -37,8 +46,6 @@ const Panel = {
           <a href={{url}} class="flex-grow jsmenu__itemlink">{{title}}</a>
           {{#button}}
             <button class="jsmenu__button--forward " value={{id}} />
-
-
           {{/button}}
         </li>
       {{/items}}
@@ -57,8 +64,8 @@ const Panel = {
     return document.getElementById(this.selectors.rootId);
   },
   content:[],
-  getPanels : function(direction){
-    // append button resolver to view data
+  getPanels : function(state){
+
     return this.content.map( (item,i) => ({
       ...item,
       button,
@@ -72,11 +79,10 @@ const Panel = {
          * At render, -up and -down classes are added and removed accordingly to achieve wanted animation and final state.
          */
         'jsmenu__panel':true,
-        // 'jsmenu__panel--first':i===0,
         'jsmenu__panel--visible':true,
         'jsmenu__panel--current':i === this.current,
-        'jsmenu__panel--visible-up':  (direction === 'start' && i > this.current ) || (direction === 'up' && i >= this.current ) ||( direction === 'down' && i > this.current+1 ),
-        'jsmenu__panel--visible-down': (direction  === 'up' && i<this.current-1)  || (direction === 'down' && i <= this.current)
+        'jsmenu__panel--visible-right':  (state === 'start' && i > this.current ) || (state === 'up' && i >= this.current ) ||( state === 'down' && i > this.current+1 ),
+        'jsmenu__panel--visible-left': (state  === 'up' && i<this.current-1)  || (state === 'down' && i <= this.current)
       })
     }));
   },
@@ -106,11 +112,11 @@ const Panel = {
     this.current = this.current - 1 >= 0 ? this.current - 1 : this.current;
     this.render('down');
   },
-  render:function(direction) {
+  render:function(state) {
     const root = this.getRoot();
     // root.parentElement.scrollTo(0,0);
     root.innerHTML = Mustache.render(this.templates.panelTemplate, {
-      panels: this.getPanels(direction),
+      panels: this.getPanels(state),
       frontpage:Drupal.t('Front Page','Mobile Menu'),
       // current:()=> this.current+1,
       back: ()=> ['back to',this.current +1].join(' ') ,
@@ -119,108 +125,107 @@ const Panel = {
     });
     const panels = [...root.querySelectorAll('.jsmenu__panel')];
     const current =  panels.at(this.current);
-    const TRESHOLD = 100;
 
+    //Scroll to back-button height if back-button is not visible any more
+    // Todo: bind treshold to back-button position when all menu blocks have been added and styled
+    const TRESHOLD = 100;
     if(root.parentElement.scrollTop > TRESHOLD) {
       current.querySelectorAll('.jsmenu__button--back')[0].scrollIntoView({block:'start',behaviour:'smooth'});
-      // console.log('scroll treshold exceeded',{TRESHOLD,sc:root.parentElement.scrollTop})
-      // root.parentElement.scrollTo({top: TRESHOLD} );
-      // root.parentElement.scrollTo({top: 0, behavior: 'smooth'});
     }
-
 
     setTimeout(()=>{
 
       //element has multiple transitions so we dont want this to run on all of them.
-      const afterTransitions =({propertyName})=> {
-        //run only on transform transition, should be only one. If this
-        // becomes too tricky with CSS, use once() and just dont care about it.
-        if(propertyName !== 'opacity') {
-          return;
-        }
-        // if(root.parentElement.scrollTop > TRESHOLD) {
-        //   current.querySelectorAll('.jsmenu__button--back')[0].scrollIntoView({block:'start',behaviour:'smooth'});
-        //   // console.log('scroll treshold exceeded',{TRESHOLD,sc:root.parentElement.scrollTop})
-        //   // root.parentElement.scrollTo({top: TRESHOLD} );
-        //   // root.parentElement.scrollTo({top: 0, behavior: 'smooth'});
-        // }
-        // if(root.parentElement.scrollTop > TRESHOLD && current.querySelectorAll('.jsmenu__panel-body')[0].clientHeight < window.innerHeight*0.7 ) {
-        //   console.log({TRESHOLD,sc:root.parentElement.scrollTop})
-        //   root.parentElement.scrollTo({top: 0, behavior: 'smooth'});
-        // }
-        // current.querySelectorAll('.jsmenu__button--back')[0].scrollIntoView({block:'start',behaviour:'smooth'});
-        // if(root.parentElement.scrollTop > 0 ) {
-        //   current.querySelectorAll('.jsmenu__button--back')[0].scrollIntoView({block:'start',behaviour:'smooth'});
-        // }
-
-      };
+      // const afterTransitions =({propertyName})=> {
+      //   //run only on transform transition, should be only one. If this
+      //   // becomes too tricky with CSS, use once() and just dont care about it.
+      //   if(propertyName !== 'opacity') {
+      //     return;
+      //   }
+      // };
 
 
       current.classList.add('jsmenu__panel--visible-fast');
-      current.classList.remove('jsmenu__panel--visible-up','jsmenu__panel--visible-down');
-      switch (direction) {
+      current.classList.remove('jsmenu__panel--visible-right','jsmenu__panel--visible-left');
+      switch (state) {
       /***
        *  Exiting panel moves slow and fades away from given direction
        *  Entering panel moves fast and enters stage from given direction
        */
 
       case 'up':
-        panels.at(this.current-1).classList.add('jsmenu__panel--visible-down','jsmenu__panel--visible-slow');
-        panels.at(this.current-1).addEventListener('transitionend',afterTransitions);
+        panels.at(this.current-1).classList.add('jsmenu__panel--visible-left','jsmenu__panel--visible-slow');
+        // panels.at(this.current-1).addEventListener('transitionend',afterTransitions);
         break;
 
       case 'down':
-        panels.at(this.current+1).classList.add('jsmenu__panel--visible-up', 'jsmenu__panel--visible-slow');
-        panels.at(this.current+1).addEventListener('transitionend',afterTransitions);
+        panels.at(this.current+1).classList.add('jsmenu__panel--visible-right', 'jsmenu__panel--visible-slow');
+        // panels.at(this.current+1).addEventListener('transitionend',afterTransitions);
         break;
 
       default:
-        // 'start does not need a handler
 
       }
 
     },10);
+  },
+  load: async function(){
+    const MENU = await( await fetch('./megamenu.json')).json();
+    this.content = [MENU];
+  },
+  start: async function(){
+    console.log('start');
+    if(!this.getRoot()) {
+      console.error('menu data not loaded');
+      throw new Error('Panel root not found');
+
+    }
+    this.render('load');
+    try {
+      await this.load();
+    } catch(e) {
+      console.error(e);
+      // throw e;
+      this.content = [mockmenu, ...Array(4)];
+
+    }
+
+    this.render('start');
+    this.getRoot().addEventListener('click', ({
+      target: {
+        classList,
+        value:id
+      }
+    }) => {
+
+      /**
+       * Event listeners:
+       *
+       *  Bind one click event listener to main panel. One for all click events.
+       *  Add more if one handler becomes too cumbersome.
+       *  */
+
+      if (classList && classList.contains(this.selectors.forward)) {
+        this.up(id);
+      } else if (classList && classList.contains(this.selectors.back)) {
+        this.down();
+      }
+    });
+
+
+
 
 
   }
 };
 
-document.addEventListener('DOMContentLoaded', async function() {
 
-  if(!Panel.getRoot()) {
-    throw new Error('Panel root not found');
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  // TODO integrate with megamenu button
+  const toggleButton = '.cssmenu-toggle';
+  const start = once(()=>Panel.start());
+  document.querySelectorAll(toggleButton)[0].addEventListener('click',start);
 
-  try {
-    const MENU = await( await fetch('./megamenu.json')).json();
-    Panel.content = [MENU];
-  } catch (error) {
-    console.error(error);
-    // throw error
-    Panel.content = [mockmenu, ...Array(4)];
-  }
-
-  Panel.render('start');
-
-  /**
-   * Event listeners:
-   *
-   *  Bind one click event listener to main panel. One for all click events.
-   *  Add more if one handler becomes too cumbersome.
-   *  */
-  Panel.getRoot().addEventListener('click', function({
-    target: {
-      classList,
-      value:id
-    }
-  }) {
-
-    if (classList && classList.contains(Panel.selectors.forward)) {
-      Panel.up(id);
-    } else if (classList && classList.contains(Panel.selectors.back)) {
-      Panel.down();
-    }
-  });
 });
 
 
