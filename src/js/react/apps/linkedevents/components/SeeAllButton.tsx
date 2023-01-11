df736c2a9f2ba4035514763c726aadf579123520
Jussiles <juss.leskinen@gmail.com>
2023-01-11 16:44:31 +0200

    UHF-7791: UHF-7791: see all button. Todos for data attributes.

--- src/js/react/apps/linkedevents/components/SeeAllButton.tsx
+++ src/js/react/apps/linkedevents/components/SeeAllButton.tsx
@@ -2,14 +2,22 @@ import ROOT_ID from '../enum/RootId';
 
 function SeeAllButton() {
   const rootElement: HTMLElement | null = document.getElementById(ROOT_ID);
+  // TODO: add API EVENT URL as data attribute ti get tapahtumat.hel.fi url with params
   const eventsUrl = rootElement?.dataset?.eventsUrl;
 
   return (
     <a
-      className='event-list__see-all-button'
       href={eventsUrl}
+      className='hds-button hds-button--link hds-button--secondary'
+      data-is-external="true"
     >
-      {Drupal.t('Tarkenna hakua tapahtumat.hel.fi:ssä')}
+      <span className="hds-button__label">{Drupal.t('Refine search in tapahtumat.hel.fi')}</span>
+      <span className="link__type link__type--external" aria-label={`(${Drupal.t(
+          'Link leads to external service',
+          {},
+          { context: 'Explanation for screen-reader software that the icon visible next to this link means that the link leads to an external service.' }
+        )})`}>
+      </span>
     </a>
   );
 }
