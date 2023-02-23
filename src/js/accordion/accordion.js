1d2ec6db2ba76b12ea446b359e9215adb5dd2c8c
rpnykanen <risto.nykanen@druid.fi>
2023-02-23 12:33:47 +0200

    open accordion if anchor exists inside accordion

--- src/js/accordion/accordion.js
+++ src/js/accordion/accordion.js
@@ -1,34 +1,54 @@
 import HelfiAccordion from './helfi-accordion';
 import State from './state';
 import { createListOfUniqueIds } from './unique-id';
+import AccordionItem from './accordion-item';
 
-const accordionElements = document.querySelectorAll(`.${HelfiAccordion.accordionWrapper}`);
+// const accordionElements = document.querySelectorAll(`.${HelfiAccordion.accordionWrapper}`);
 
+/*
 const allAccordionItemIds = [...accordionElements]
   .map(accordion => Array.from(accordion.getElementsByClassName('helfi-accordion-item')).map(item => item.dataset.accordionId))
   .reduce((accumulator, currentValue) => accumulator.concat(...currentValue), []);
+*/
 
 // Add suffix to duplicate ids.
-const uniqueListOfIds = createListOfUniqueIds(allAccordionItemIds);
+// const uniqueListOfIds = createListOfUniqueIds(allAccordionItemIds);
 
 // Replace all duplicate ids across all accordions.
+/*
 [...accordionElements].map(accordion => Array.from(accordion.getElementsByClassName('helfi-accordion-item')))
   .reduce((accumulator, currentValue) => accumulator.concat(...currentValue), [])
   .forEach((element, index) => {
     element.querySelector('.helfi-accordion__header').setAttribute('id', uniqueListOfIds[index]);
   });
-
+*/
 
 // Listen to hash change.
 window.addEventListener('hashchange', (event) => {
   const {hash} = window.location;
 
+  let accordionItemFound = false;
   window.helfiAccordions.forEach((accordion) => {
     const accordionItem = accordion.getAccordionItemById(hash.replace('#', ''));
     if (accordionItem) {
+      accordionItemFound = true;
       accordionItem.open();
     }
   });
+
+  if (!accordionItemFound) {
+    const anchorElement = document.querySelector(hash);
+    const accordionItemToFind = anchorElement.closest(`.${AccordionItem.accordionItemElement}`);
+    if (accordionItemToFind) {
+      window.helfiAccordions.forEach((accordion) => {
+        const accordionItem = accordion.getAccordionItemById(accordionItemToFind.getId());
+        if (accordionItem) {
+          accordionItem.open();
+        }
+      });
+    }
+  }
+
 });
 
 // Initialize the accordions
