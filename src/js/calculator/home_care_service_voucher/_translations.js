9f55cd3e0f6d8df91fa2a8525bdb7b6806cc689f
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2023-03-07 10:43:03 +0200

    UHF-7482: Add initial version of homecare service voucher

---
+++ src/js/calculator/home_care_service_voucher/_translations.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-template-curly-in-string */
+
+const translations = {
+  household_size: {
+    fi: 'Talouden koko',
+    sv: null,
+    en: null,
+  },
+  household_size_explanation: {
+    fi: 'Samassa osoitteessa asuvien perheenjäsenten määrä.',
+    sv: null,
+    en: null,
+  },
+  gross_income_per_month: {
+    fi: 'Talouden yhteenlasketut tulot kuukaudessa',
+    sv: null,
+    en: null,
+  },
+  gross_income_per_month_explanation: {
+    fi: 'Kuukausitulot tarkoittavat bruttotuloja eli tuloja ennen verojen vähentämistä. Tuloihin lasketaan palkka, eläke-, vuokra- ja korkotulot sekä etuudet, esimerkiksi eläkkeensaajan hoitotuki.',
+    sv: null,
+    en: null,
+  },
+  monthly_usage: {
+    fi: 'Kotihoidon tuntimäärä kuukaudessa',
+    sv: null,
+    en: null,
+  },
+  monthly_usage_explanation: {
+    fi: 'Löydät tuntimäärän kotihoidon hoito- ja palvelusuunnitelmastasi.',
+    sv: null,
+    en: null,
+  },
+  service_provider_price: {
+    fi: 'Palveluntuottajan tuntihinta',
+    sv: null,
+    en: null,
+  },
+  service_provider_price_explanation: {
+    fi: 'Valitsemasi palveluntuottajan perimä kotihoidon tuntihinta.',
+    sv: null,
+    en: null,
+  },
+
+};
+
+export default translations;
---
+++ src/js/calculator/home_care_service_voucher/home_care_service_voucher.js
@@ -0,0 +1,97 @@
+import form from './_form';
+import translations from './_translations';
+
+class HomeCareServiceVoucher {
+  constructor(id, settings) {
+    this.id = id;
+    const parsedSettings = JSON.parse(settings);
+
+    // Expecting settings to follow this JSON format:
+    /*
+    const parsedSettings = {
+    };
+    // */
+    // Form content
+    const getFormData = () => form.getFormData(this.id, this.t);
+
+    const update = () => {};
+
+
+    const validate = () => {
+      const errorMessages = [];
+
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // Validate basics from form
+      errorMessages.push(...this.calculator.validateBasics('household_size'));
+      errorMessages.push(...this.calculator.validateBasics('gross_income_per_month'));
+      errorMessages.push(...this.calculator.validateBasics('monthly_usage'));
+      errorMessages.push(...this.calculator.validateBasics('service_provider_price'));
+
+      // Check if any missing input errors were found
+      if (errorMessages.length) {
+        return {
+          error: {
+            title: this.t('missing_input'),
+            message: errorMessages
+          },
+        };
+      }
+
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // Get fielf values for calculating.
+      const householdSize = Number(this.calculator.getFieldValue('household_size'));
+      const grossIncomePerMonth = Number(this.calculator.getFieldValue('gross_income_per_month'));
+      const grossIncomePerMonthRaw = this.calculator.getFieldValue('gross_income_per_month');
+      const monthlyUsage = Number(this.calculator.getFieldValue('monthly_usage'));
+      const serviceProviderPrice = Number(this.calculator.getFieldValue('service_provider_price'));
+
+
+      return {
+        // receipt,
+        // ariaLive: this.t('receipt_aria_live', { payment: sum }),
+      };
+    };
+
+    const eventHandlers = {
+      submit: (event) => {
+        this.calculator.clearResult();
+        event.preventDefault();
+        const result = validate();
+        this.calculator.renderResult(result);
+      },
+      keydown: () => {
+        update();
+        // this.calculator.clearResult();
+      },
+      change: () => {
+        update();
+        // this.calculator.clearResult();
+        // validate();
+      },
+      reset: () => {
+        window.setTimeout(update, 1);
+        this.calculator.clearResult();
+        this.calculator.showAriaLiveText(this.t('reset_aria_live'));
+      },
+    };
+
+    // Prepare calculator for translations
+    this.calculator = window.HelfiCalculator({ name: 'home_care_service_voucher', translations });
+
+    // Create shortcut for translations
+    this.t = (key, value) => this.calculator.translate(key, value);
+
+    // Parse settings to js
+    this.settings = this.calculator.parseSettings(settings);
+
+    // Initialize calculator
+    this.calculator.init({
+      id,
+      formData: getFormData(),
+      eventHandlers,
+    });
+  }
+}
+
+window.helfi_calculator = window.helfi_calculator || {};
+window.helfi_calculator.home_care_service_voucher = (id, settings) => new HomeCareServiceVoucher(id, settings);
