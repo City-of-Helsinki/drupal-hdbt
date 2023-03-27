097ba6d1e9e27df7fd6f4041d40d8d859dc5305e
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2023-03-27 15:46:19 +0300

    UHF-8164: Add Swedish translations to daycare

--- src/js/calculator/_globalTranslations.js
+++ src/js/calculator/_globalTranslations.js
@@ -3,13 +3,13 @@
 const globalTranslations = {
   has_required_fields: {
     fi: 'Pakolliset kentät on merkitty tähdellä ${required}.',
-    sv: null,
+    sv: 'Obligatoriska fält är markerade med en asterisk ${required}.',
     en: 'Required fields are indicated with an asterisk ${required}.',
   },
   not_saved: {
     fi: 'Huomioithan, että laskuriin antamiasi tietoja ei tallenneta eikä lähetetä eteenpäin. Jos muutat laskurin tietoja, muista painaa "Laske arvio" uudelleen.',
-    sv: null,
-    en: 'Please note that the information you enter will not be stored or forwarded.If you change any of the calculator information, remember to click "Calculate estimate" again.',
+    sv: 'Observera att de uppgifter som du anger i räknaren inte sparas eller skickas vidare. Om du ändrar uppgifterna i räknaren ska du komma ihåg att klicka på "Beräkna uppskattningen" på nytt.',
+    en: 'Please note that the information you enter will not be stored or forwarded. If you change any of the calculator information, remember to click "Calculate estimate" again.',
   },
   unit_euro: {
     fi: 'euroa',
@@ -19,12 +19,12 @@ const globalTranslations = {
   unit_person: {
     fi: 'henkilöä',
     sv: 'i personer',
-    en: 'persons',
+    en: 'in persons',
   },
   unit_day: {
     fi: 'päivää',
-    sv: null,
-    en: 'days',
+    sv: 'i dagar',
+    en: 'in days',
   },
   unit_hour: {
     fi: 'tuntia',
@@ -33,18 +33,18 @@ const globalTranslations = {
   },
   unit_amount: {
     fi: 'kpl',
-    sv: null,
-    en: null,
+    sv: 'st',
+    en: 'pcs',
   },
   required: {
     fi: '(Pakollinen kenttä)',
-    sv: null,
+    sv: '(Obligatoriskt fält)',
     en: '(Required field)',
   },
   required_explanation: {
     fi: 'sekä saavutettavalla tekstillä ',
-    sv: null,
-    en: null,
+    sv: 'och med tillgänglig text ',
+    en: 'and with accessible text ',
   },
   calculate: {
     fi: 'Laske arvio',
@@ -53,18 +53,18 @@ const globalTranslations = {
   },
   reset: {
     fi: 'Tyhjennä tiedot',
-    sv: null,
+    sv: 'Radera uppgifterna',
     en: 'Clear data',
   },
   reset_aria_live: {
     fi: 'Lomakkeen tiedot on nyt tyhjennetty',
-    sv: null,
-    en: 'The data on the form has now been cleared.',
+    sv: 'Uppgifterna har nu raderats från blanketten',
+    en: 'The data on the form has now been cleared',
   },
   missing_input: {
     fi: 'Tarkistathan vielä nämä kohdat',
-    sv: null,
-    en: 'Please check these items.',
+    sv: 'Kontrollera ännu dessa punkter',
+    en: 'Please check these items',
   },
   error_with_link: {
     fi: '${error}, ${labelLink}',
@@ -73,57 +73,57 @@ const globalTranslations = {
   },
   select_radio: {
     fi: 'Valinta on pakollinen',
-    sv: null,
-    en: 'Selection is required.',
+    sv: 'Valet är obligatoriskt',
+    en: 'Selection is required',
   },
   enter_value: {
     fi: 'Kenttä on pakollinen',
-    sv: null,
-    en: 'Field is required.',
+    sv: 'Fältet är obligatoriskt',
+    en: 'Field is required',
   },
   must_be_number: {
     fi: 'Kenttään on syötettävä numero',
-    sv: null,
-    en: 'You must enter a number in the field.',
+    sv: 'Du måste ange en siffra i fältet',
+    en: 'You must enter a number in the field',
   },
   must_be_whole_number: {
     fi: 'Kenttään on syötettävä kokonaisluku',
-    sv: null,
-    en: 'You must enter a whole number in the field.',
+    sv: 'Du måste ange ett heltal i fältet',
+    en: 'You must enter a whole number in the field',
   },
   min_or_max_out_of_bounds: {
     fi: 'Arvon pitää olla väliltä ${min} ja ${max}',
-    sv: null,
+    sv: 'Siffran måste vara mellan ${min} och ${max}',
     en: 'The value must be between ${min} and ${max}',
   },
   min_out_of_bounds: {
     fi: 'Arvon pitää olla ${min} tai enemmän',
-    sv: null,
-    en: 'The value must be ${min} or more.',
+    sv: 'Siffran måste vara ${min} eller mer',
+    en: 'The value must be ${min} or more',
   },
   max_out_of_bounds: {
     fi: 'Arvon pitää olla ${max} tai vähemmän',
-    sv: null,
-    en: 'The value must be ${max} or less.',
+    sv: 'Siffran måste vara ${max} eller mindre',
+    en: 'The value must be ${max} or less',
   },
   result: {
     fi: 'Lopputulos',
-    sv: null,
+    sv: 'Resultat',
     en: 'Result',
   },
   notification_aria_label_for_error: {
     fi: 'Virheilmoitus',
-    sv: null,
+    sv: 'Felanmälan',
     en: 'Error message',
   },
   notification_aria_label_for_alert: {
     fi: 'Varoitus',
-    sv: null,
+    sv: 'Varning',
     en: 'Warning',
   },
   notification_aria_label_for_info: {
     fi: 'Tiedoksi',
-    sv: null,
+    sv: 'För kännedom',
     en: 'For information',
   },
 };
