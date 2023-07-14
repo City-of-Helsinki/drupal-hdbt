718e7874813e4fd6f33c5ec1678446e2b9d3819b
Krister Haverinen <krister.haverinen@siili.com>
2023-07-14 14:11:17 +0300

    UHF-8590: Fix aria-hidden

--- src/js/react/apps/linkedevents/containers/SelectionsContainer.tsx
+++ src/js/react/apps/linkedevents/containers/SelectionsContainer.tsx
@@ -207,7 +207,7 @@ const ClearButton = ({ showClearButton, url }: ClearButtonProps) => {
   return (
     <li className='hdbt-search__clear-all'>
       <Button
-        aria-hidden={showClearButton ? 'true' : 'false'}
+        aria-hidden={showClearButton ? 'false' : 'true'}
         className='hdbt-search__clear-all-button'
         iconLeft={<IconCross className='hdbt-search__clear-all-icon' />}
         onClick={resetForm}
