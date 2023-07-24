439ee2fe109370e5d87339445c01b08da4b7c0fb
rpnykanen <risto.nykanen@druid.fi>
2023-07-24 16:22:20 +0300

    UHF-8737: rest of the things in their own preprocesses

--- modules/hdbt_default/src/Entity/HelfiMediaBase.php
+++ modules/hdbt_default/src/Entity/HelfiMediaBase.php
@@ -12,31 +12,12 @@ class HelfiMediaBase extends Media implements MediaInterface {
     return helfi_eu_cookie_compliance_get_privacy_policy_url();
   }
 
-  public function getIframeTitle(): ?string {
-    if (!$this->hasProvider()) {
-      return NULL;
-    }
-    return $this->get('field_media_oembed_video')
-      ->iframe_title;
-  }
-
   public function getJsLibrary(): string {
     return 'hdbt/embedded-content-cookie-compliance';
-  }
-
-  public function getServiceUrl(): ?string {
-    if (!$this->hasProvider()) {
-      return NULL;
-    }
-    $url_resolver = \Drupal::service('media.oembed.url_resolver');
-    $video_url = $this->get('field_media_oembed_video')->value;
-    $provider = $url_resolver->getProviderByUrl($video_url);
-    return rtrim($provider->getUrl(), '/');
   }
 
   protected function hasProvider(): bool{
     return \Drupal::moduleHandler()->moduleExists('oembed_providers') &&
       $this->hasField('field_media_oembed_video');
   }
-
 }
--- modules/hdbt_default/src/Entity/RemoteVideo.php
+++ modules/hdbt_default/src/Entity/RemoteVideo.php
@@ -3,4 +3,19 @@
 namespace Drupal\hdbt_default\Entity;
 
 class RemoteVideo extends HelfiMediaBase {
+  public function getServiceUrl(): ?string {
+    if (!$this->hasProvider()) {
+      return NULL;
+    }
+    $url_resolver = \Drupal::service('media.oembed.url_resolver');
+    $video_url = $this->get('field_media_oembed_video')->value;
+    $provider = $url_resolver->getProviderByUrl($video_url);
+    return rtrim($provider->getUrl(), '/');
+  }
+
+  public function getMediaTitle() {
+    return $this->get('field_media_oembed_video')
+      ->iframe_title;
+  }
+
 }
