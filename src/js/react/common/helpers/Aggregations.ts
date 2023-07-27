4daa16c94e859a7212b61676a5a820611dddd3d5
Jere Ljungberg <jere@mysteerio.fi>
2023-07-27 15:25:53 +0300

    UHF-8535: Move custom agg logic to job-search

--- src/js/react/common/helpers/Aggregations.ts
+++ src/js/react/common/helpers/Aggregations.ts
@@ -5,12 +5,7 @@ const bucketToMap = (bucket: AggregationItem[] = []) => {
   const result = new Map();
 
   bucket.forEach(item => {
-    if (item?.unique?.value) {
-      result.set(item.key, item.unique.value);
-    }
-    else {
-      result.set(item.key, item.doc_count);
-    }
+    result.set(item.key, item.doc_count);
   });
 
   return result;
