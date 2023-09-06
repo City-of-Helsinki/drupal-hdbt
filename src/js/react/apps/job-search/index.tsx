3a73792de425fd7bca076c2d5b60d67d02c68577
Jussiles <juss.leskinen@gmail.com>
2023-09-06 15:04:32 +0300

    UHF-8081: refactoring.

--- src/js/react/apps/job-search/index.tsx
+++ src/js/react/apps/job-search/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import initSentry from '@/react/common/helpers/Sentry';
 import SearchContainer from './containers/SearchContainer';
-import initSentry from './helpers/Sentry';
 
 initSentry();
 
