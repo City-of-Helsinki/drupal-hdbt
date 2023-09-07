9bed4119f539728bcd836a5d7a8fb92bcd9e76e5
Jussiles <juss.leskinen@gmail.com>
2023-09-07 13:17:07 +0300

    UHF-8081: UHF-8081: clean and add sentry exception to job search.

--- src/js/react/apps/job-search/components/results/ResultsError.tsx
+++ src/js/react/apps/job-search/components/results/ResultsError.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { ForwardedRef, forwardRef } from 'react';
 
 type ResultsErrorProps = {
@@ -6,6 +7,7 @@ type ResultsErrorProps = {
 
 const ResultsError = forwardRef(({ error }: ResultsErrorProps, ref: ForwardedRef<HTMLDivElement> ) => {
   console.warn(`Error loading data. ${error}`);
+  Sentry.captureException(error);
   return (
     <div className='job-search__results' ref={ref}>
       {Drupal.t('The website encountered an unexpected error. Please try again later.')}
--- src/js/react/apps/job-search/components/results/ResultsList.tsx
+++ src/js/react/apps/job-search/components/results/ResultsList.tsx
@@ -1,5 +1,5 @@
-import Job from '../../types/Job';
 import Result from '@/types/Result';
+import Job from '../../types/Job';
 import ResultCard from './ResultCard';
 
 type ResultsListProps = {
@@ -7,15 +7,15 @@ type ResultsListProps = {
 };
 
 const ResultsList = ({ hits }: ResultsListProps) => (
-    <>
-      {hits.map(hit => (
-        <ResultCard
-          key={hit._id}
-          innerHits={hit?.inner_hits.translations.hits.hits || null}
-          job={hit._source}
-        />
-      ))}
-    </>
-  ); 
+  <>
+    {hits.map(hit => (
+      <ResultCard
+        key={hit._id}
+        innerHits={hit?.inner_hits.translations.hits.hits || null}
+        job={hit._source}
+      />
+    ))}
+  </>
+);
 
 export default ResultsList;
