aeb239448f546ec5f917f62416f3977ea70be360
Jussiles <juss.leskinen@gmail.com>
2023-09-12 08:35:41 +0300

    UHF-8211: UHF-8081: add common error message to linkedevents search.

--- src/js/react/common/ResultsError.tsx
+++ src/js/react/common/ResultsError.tsx
@@ -3,12 +3,16 @@ import { ForwardedRef, forwardRef } from 'react';
 
 type ResultsErrorProps = {
   error: string|Error;
-  className: string;
+  className?: string;
 }
 
 const ResultsError = forwardRef(({ error, className }: ResultsErrorProps, ref: ForwardedRef<HTMLDivElement>) => {
   console.warn(`Error loading data. ${error}`);
-  Sentry.captureException(error);
+
+  if (drupalSettings?.helfi_react_search?.sentry_dsn_react) {
+    Sentry.captureException(error);
+  }
+
   return (
     <div className={className} ref={ref}>
       {Drupal.t('An error occured while loading the content. Please reload page.', {}, { context: 'React search' })}
