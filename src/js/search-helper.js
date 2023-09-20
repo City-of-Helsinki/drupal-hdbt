c28a390a79a6447e41ab848c979a4b793737c920
Krister Haverinen <krister.haverinen@siili.com>
2023-09-20 14:27:15 +0300

    UHF-8839: Add view id indentifier for selector

--- src/js/search-helper.js
+++ src/js/search-helper.js
@@ -2,7 +2,11 @@
 
   // Set focus on search result count
   $(document).ajaxComplete(function onDataLoaded(e, xhr, settings) {
-    const resultCountEl = $('.unit-search__count-container')[0];
+    // Check form ID to prevent mixing on multi-form page
+    const viewDomId = settings.extraData.view_dom_id;
+    const resultsContainerEl = $(`[data-id-number=${  viewDomId  }]`);
+    const resultCountEl = $(resultsContainerEl[0]).find('.unit-search__count-container')[0];
+    
     if (!resultCountEl) return;
     resultCountEl.setAttribute('tabindex', '-1');
     resultCountEl.focus();
