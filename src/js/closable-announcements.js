f6bed5fc9dccdbcdcc6f455931661b10864e1616
Tero Elonen <tero.elonen@druid.fi>
2023-09-21 13:39:08 +0300

    UHF-8930: Add javascript that moves focus to previous focusable element when annoucement is closed, add announcement close button title for screen readers, move close button as first focusable element on announcement

--- src/js/closable-announcements.js
+++ src/js/closable-announcements.js
@@ -29,9 +29,39 @@
 
       function closeAnnouncement(announcement) {
         addToAnnouncementStorage(announcement);
-        announcement.style = `--js-announcement-height: ${announcement.offsetHeight}px`; // Set fixed height to allow CSS animation
+        // Set fixed height to allow CSS animation
+        announcement.style = `--js-announcement-height: ${announcement.offsetHeight}px`;
         window.setTimeout(() => {
           announcement.classList.add(ANNOUCEMENT_HIDE);
+          // Moving focus to correct place after closing the announcement.
+          // NOTICE: This is very dependent on the html-structure of the header
+          // and its components.
+          // If previous sibling for the closed announcement is another
+          // announcement we move the focus there.
+          if (
+            announcement.previousElementSibling &&
+            announcement.previousElementSibling.matches('.announcement:not(.js-announcement--hide)')
+          ) {
+            const focusableAnnouncementElements = announcement.previousElementSibling.querySelectorAll('.announcement__close, .announcement__link a');
+            focusableAnnouncementElements[focusableAnnouncementElements.length - 1].focus();
+          } else {
+            // If previous sibling is not announcement we need to figure out
+            // if we are viewing the page with mobile or desktop navigation. We
+            // check if the header-bottom menu is visible to determine this.
+            const desktopMenu = document.querySelectorAll('.header-bottom .desktop-menu > .menu');
+            for (let i = 0, max = desktopMenu.length; i < max; i++) {
+              const desktopMenuHidden = window.getComputedStyle(desktopMenu[i]).display === 'none';
+              // Depending on the visible menu we move the focus straight to
+              // nav-toggle buttons or the menu links if no breadcrumb is set.
+              if (desktopMenuHidden === true) {
+                const focusableElements = document.querySelectorAll('.nav-toggle__button, .breadcrumb a:last-of-type');
+                focusableElements[focusableElements.length - 1].focus();
+              } else {
+                const focusableElements = document.querySelectorAll('.nav-toggle__button, .header-bottom .menu--level-0 > .menu__item:last-child > .menu__link-wrapper :where(a, button), .breadcrumb a:last-of-type');
+                focusableElements[focusableElements.length - 1].focus();
+              }
+            }
+          }
         }, 1); // Delay setting class a bit to allow css accept fixed height before animating
       }
 
