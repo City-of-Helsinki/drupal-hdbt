d27b9fd245ecd3762592fc7ddf067686493dc6a7
Krister Haverinen <krister.haverinen@siili.com>
2023-09-21 14:59:06 +0300

    UHF-8839: Update selector to match all templates

--- src/js/search-helper.js
+++ src/js/search-helper.js
@@ -5,7 +5,7 @@
     // Check form ID to prevent mixing on multi-form page
     const viewDomId = settings.extraData.view_dom_id;
     const resultsContainerEl = $(`[data-id-number=${  viewDomId  }]`);
-    const resultCountEl = $(resultsContainerEl[0]).find('.unit-search__count-container')[0];
+    const resultCountEl = $('[class$="__count-container"]', resultsContainerEl[0])[0];
     
     if (!resultCountEl) return;
     resultCountEl.setAttribute('tabindex', '-1');
