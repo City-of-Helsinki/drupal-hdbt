06dbb0b4bde6b1cfc44f9ba857af612e775905ad
Tero Elonen <tero.elonen@druid.fi>
2024-01-30 17:21:59 +0200

    UHF-9327: Change school search pager to display 15 items per page

--- src/js/react/apps/school-search/enum/AppSettings.ts
+++ src/js/react/apps/school-search/enum/AppSettings.ts
@@ -1,7 +1,7 @@
 const AppSettings = {
   index: 'schools',
   locationsBaseUrl: 'https://api.hel.fi/servicemap/v2/administrative_division/?municipality=helsinki&unit_include=id&type=lower_comprehensive_school_district_fi,lower_comprehensive_school_district_sv,upper_comprehensive_school_district_fi,upper_comprehensive_school_district_sv',
-  size: 10
+  size: 15
 };
 
 export default AppSettings;
