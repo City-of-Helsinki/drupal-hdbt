0fe0717790cb53757af6a00e2408577a7f54ef56
j-mys <jusa@mysteerio.fi>
2024-03-05 14:56:08 +0200

    UHF-9675: add error handling, improve code quality

--- src/js/hds-cc/template.js
+++ src/js/hds-cc/template.js
@@ -86,7 +86,7 @@ export function getGroupHtml(translations, groupId, tableRowsHtml, groupRequired
                 <span aria-hidden="true" class="hds-icon hds-icon--angle-down"></span>
               </button>
               <div class="hds-cc__group-details" id="hds-cc-group-details-1">
-                <div class="hds-cc__group-details__animator hds-table-container" tabindex="0" role="region" aria-label="FIXME - is this needed? Cookies related to necessary cookies">
+                <div class="hds-cc__group-details__animator hds-table-container" tabindex="0" role="region">
                   <table class="hds-table hds-table--dark">
                     <thead>
                       <tr class="hds-table__header-row">
