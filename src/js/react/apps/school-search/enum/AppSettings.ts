6b50d31800e82a60a9152d682994ad1b0eaf6286
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-03-22 08:51:23 +0200

    UHF-9569: UHF-9569: Fix deprecated usage of administrative_division endpoint
    service_point_id- and unit-fields will be removed in the future. They
    should be replaced with the use of "units" field. The deprecated fields
    will stop appearing in the api results some time soon.

--- src/js/react/apps/school-search/enum/AppSettings.ts
+++ src/js/react/apps/school-search/enum/AppSettings.ts
@@ -1,6 +1,6 @@
 const AppSettings = {
   index: 'schools',
-  locationsBaseUrl: 'https://api.hel.fi/servicemap/v2/administrative_division/?municipality=helsinki&unit_include=id&type=lower_comprehensive_school_district_fi,lower_comprehensive_school_district_sv,upper_comprehensive_school_district_fi,upper_comprehensive_school_district_sv',
+  locationsBaseUrl: 'https://api.hel.fi/servicemap/v2/administrative_division/?municipality=helsinki&type=lower_comprehensive_school_district_fi,lower_comprehensive_school_district_sv,upper_comprehensive_school_district_fi,upper_comprehensive_school_district_sv',
   size: 15
 };
 
