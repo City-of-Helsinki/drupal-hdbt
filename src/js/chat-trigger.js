b4a737eb693f611ed5c96eeadcb46f66e91f404f
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2024-04-10 15:17:06 +0300

    UHF-9280: UHF-9280: Refactor code to wait for chat button element

--- src/js/chat-trigger.js
+++ src/js/chat-trigger.js
@@ -1,98 +1,95 @@
-const triggerTranslations = {
+// This script replaces a chat trigger placeholder element with a button that opens the chat.
+(function chatTriggerWrapper() {
+  const triggerTranslations = {
     fallBack: {
-        fi: 'Chatti ei ole saatavilla.',
-        sv: 'Chatten är inte tillgänglig.',
-        en: 'The chat is not available.'
+      fi: 'Chatti ei ole saatavilla.',
+      sv: 'Chatten är inte tillgänglig.',
+      en: 'The chat is not available.'
     },
     openChat: {
-        fi: 'Avaa chat',
-        sv: 'Öppna chatten',
-        en: 'Open the chat'
+      fi: 'Avaa chat',
+      sv: 'Öppna chatten',
+      en: 'Open the chat'
     }
-};
+  };
 
-// Due to current ckeditor rules the custom element
-// or custom property button are not available,
-// this will insert the chat trigger element to page.
-function addCustomTrigger(working = true) {
-    const trigger = document.createElement('chat-trigger');
-    if (working) {
-        trigger.setAttribute('data-chat-trigger', 'aca-wbc-chat-app-button');
-    } else {
-        trigger.setAttribute('data-chat-trigger', 'this does not work');
+  /**
+   * Waits for an element to appear in the DOM and resolves with the element.
+   *
+   * @param {string} selector - The CSS selector of the element to wait for.
+   * @return {Promise<Element>} A promise that resolves with the element once it appears in the DOM.
+   */
+  function waitForElement(selector) {
+    return new Promise(resolve => {
+      // First check, if the element exists already.
+      const element = document.querySelector(selector);
+      if (element) {
+        resolve(element);
+      } else { // If not, wait for it to appear.
+        const observer = new MutationObserver(() => {
+          const elem = document.querySelector(selector);
+          if (elem) {
+            observer.disconnect();
+            resolve(elem);
+          }
+        });
+        observer.observe(document.documentElement, { childList: true, subtree: true, });
+      }
+    });
+  }
+
+  /**
+   * A temporary fix function until CKEditor allows <chat-trigger data-chat-trigger="#aca-wbc-chat-app-button button">
+   * Replaces a placeholder element with a chat trigger element.
+   */
+  function temporaryFix() {
+    const placeholder = document.querySelector('a[href^="https://www.hel.fi/chat-trigger?click="]');
+    if (placeholder) {
+      const chatTrigger = document.createElement('chat-trigger');
+      chatTrigger.dataset.chatTrigger = decodeURIComponent(placeholder.getAttribute('href').split('?click=')[1]);;
+      placeholder.replaceWith(chatTrigger);
     }
-    trigger.textContent = 'Trigger element content; I will disappear';
-    document.querySelector('.component__content.columns--50-50 div').appendChild(trigger);
-}
+  }
 
-function init() {
+  /**
+   * Try to find the chat trigger placeholder element and replace it with a button that opens the chat.
+   */
+  function init() {
+    temporaryFix(); // TODO: Remove this temporary fix once CKEditor allows <chat-trigger data-chat-trigger="#aca-wbc-chat-app-button button">
+
+    // Find the chat trigger placeholder element.
+    let placeholder = document.querySelector('chat-trigger');
+
+    // If the chat trigger placeholder is not found, do nothing.
+    if (!placeholder) {
+      return;
+    }
+
+    // Get target selector from attribute
+    const targetSelector = placeholder.dataset.chatTrigger;
+
     // Get the language, fallback to english.
-    const lang = window?.drupalSettings?.path?.currentLanguage || 'fi';
+    const lang = window?.drupalSettings?.path?.currentLanguage || 'en';
 
-    // There could be a possibility of using a custom HTML tag.
-    const chatTrigger = document.querySelector('chat-trigger');
-
-    if (!chatTrigger) {
-        // This page doesn't have a chat trigger.
-        return;
+    // The trigger is present but the chat is not available, show fallback until chat is available.
+    if (!document.querySelector(targetSelector)) {
+      const fallBack = document.createElement('span');
+      fallBack.textContent = triggerTranslations.fallBack[lang];
+      placeholder.replaceWith(fallBack);
+      placeholder = fallBack;
     }
 
-    // Search for trigger target element - a chatbox.
-    const targetElementName = chatTrigger.dataset.chatTrigger;
-    if (typeof targetElementName !== 'string') {
-        // The target element name is somehow invalid
-        return;
-    }
+    // Wait for the target element to appear in the DOM and replace the trigger with a button that opens the chat.
+    waitForElement(targetSelector).then((chatButton) => {
+      const triggerButton = document.createElement('button');
+      triggerButton.textContent = triggerTranslations.openChat[lang];
+      triggerButton.setAttribute('data-hds-component', 'button');
+      triggerButton.addEventListener('click', () => {
+        chatButton.click();
+      });
+      placeholder.replaceWith(triggerButton);
+    });
+  }
 
-    const chatElement = document.querySelector(targetElementName);
-    if (!chatElement) {
-        // The trigger is present but the chat is not available,
-        // show fallback.
-        const fallBack = document.createElement('p');
-        fallBack.textContent = triggerTranslations.fallBack[lang];
-        chatTrigger.replaceWith(fallBack);
-        return;
-    }
-
-    // Create a function for clicking the chat open. Could be moved
-    // to the eventListener when the workaround becomes redundant.
-    const clickChatBox = () => {
-        // Due to the chat being live and connected, we shall not
-        // actually click the button. Using alert for feedback.
-        alert('*BLOCKS YOUR PATH*');
-        console.log(chatElement.querySelector('button'));
-        // chatElement.querySelector('button').click();
-    };
-
-    // Target element found, turn chat-trigger into a button
-    // and add an eventListener for it.
-    const chatButton = document.createElement('button');
-    chatButton.textContent = triggerTranslations.openChat[lang];
-    chatButton.addEventListener('click', clickChatBox);
-
-    // Possibly wrong styling
-    chatButton.setAttribute('data-hds-component', 'button');
-    chatTrigger.replaceWith(chatButton);
-}
-
-// The load event might not be the optimal one in production.
-// We need to make sure that the chat has time to initialize
-// before the trigger script is evaluated.
-window.addEventListener('load', () => {
-    // The console is filled with all kinds of messages,
-    // use delay to clear it before debugging.
-    const DELAY = 0;
-    setTimeout(() => {
-        console.clear();
-        init();
-    }, DELAY);
-});
-
-// Development helper to bypass ckeditor filtering.
-// Remove this and the addCustomTrigger -function
-// when the feature is finished.
-window.addEventListener('DOMContentLoaded', () => {
-    // Add trigger for correct or incorrect selector
-    addCustomTrigger(true);
-    // addCustomTrigger(false);
-});
+  window.addEventListener('DOMContentLoaded', init);
+})();
