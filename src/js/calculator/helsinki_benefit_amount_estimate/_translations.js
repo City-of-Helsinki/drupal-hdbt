30b5c75efde260c3eb7ed6a2f6d0b9937a9b9008
Sampo Tawast <sampo.tawast@gmail.com>
2024-08-29 11:22:39 +0300

    feat: introduce helsinki benefit estimate calculator

---
+++ src/js/calculator/helsinki_benefit_amount_estimate/_translations.js
@@ -0,0 +1,189 @@
+/* eslint-disable no-template-curly-in-string */
+
+const translations = {
+  common_no: {
+    fi: 'Ei',
+    sv: '',
+    en: '',
+  },
+  common_yes: {
+    fi: 'Kyllä',
+    sv: '',
+    en: '',
+  },
+  heading_employer: {
+    fi: 'Työnantajan tiedot',
+    sv: '',
+    en: '',
+  },
+  heading_employee: {
+    fi: 'Työntekijän tiedot',
+    sv: '',
+    en: '',
+  },
+  heading_pay_subsidy_information: {
+    fi: 'Muut työsuhteeseen haettavat tai myönnetyt tuet',
+    sv: '',
+    en: '',
+  },
+  text_pay_subsidy_information: {
+    fi: 'Helsinki-lisän lisäksi työsuhteeseen voidaan myöntää muita tukia kattamaan palkkaamisesta aiheutuvia kustannuksia.',
+    sv: '',
+    en: '',
+  },
+  label_company_type: {
+    fi: 'Työnantaja on',
+    sv: '',
+    en: '',
+  },
+  label_company_type_business: {
+    fi: 'Yritys',
+    sv: '',
+    en: '',
+  },
+  label_company_type_association: {
+    fi: 'Yhteisö',
+    sv: '',
+    en: '',
+  },
+  label_association_has_business_activities: {
+    fi: 'Harjoittaako yhteisö taloudellista toimintaa?',
+    sv: '',
+    en: '',
+  },
+  association_has_business_activities_text: {
+    fi: 'Taloudellinen toiminta tarkoittaa palveluiden ja tuotteiden myyntiä, joka on jatkuvaa, ansiotarkoituksessa ja kilpailuolosuhteissa tapahtuvaa. Kaikkien kolmen edellytyksen tulee täyttyä samanaikaisesti.',
+    sv: 'Nej',
+    en: 'No',
+  },
+  label_monthly_pay: {
+    fi: 'Työntekijän tuleva bruttopalkka',
+    sv: '',
+    en: '',
+  },
+  monthly_pay_helper_text: {
+    fi: 'Euroa kuukaudessa. Arvioi bruttopalkan suuruus työsuhteeseen, johon Helsinki-lisää haetaan.',
+    sv: '',
+    en: '',
+  },
+  label_vacation_money: {
+    fi: 'Lomaraha',
+    sv: '',
+    en: '',
+  },
+  vacation_money_helper_text: {
+    fi: 'Euroa kuukaudessa. Jos jätät kentän tyhjäksi, niin lomaraha jätetään huomioimatta laskelmassa.',
+    sv: '',
+    en: '',
+  },
+  label_pay_subsidy_granted: {
+    fi: 'Onko työsuhteeseen myönnetty tai haettu palkkatukea tai 55 vuotta täyttäneiden työllistämistukea?',
+    sv: '',
+    en: '',
+  },
+  label_pay_subsidy_percentage: {
+    fi: 'Mikä on palkkatuen prosentti tai myöntämisperuste?',
+    sv: '',
+    en: '',
+  },
+  label_pay_subsidy_percentage_1: {
+    fi: 'Tuki kattaa ${value} % palkkauskustannuksista (tuen perusteena ammatillisen osaamisen parantaminen)',
+    sv: '',
+    en: '',
+  },
+  label_pay_subsidy_percentage_2: {
+    fi: 'Tuki kattaa ${value} % palkkauskustannuksista (tuen perusteena alentunut työkyky tai 55 vuotta täyttäneiden työllistämistuki)',
+    sv: '',
+    en: '',
+  },
+  helper_text_pay_subsidy_percentage: {
+    fi: 'Valitse sopivin vaihtoehto joko tuen prosentin tai tukea haettavan perusteen mukaan.',
+    sv:'',
+    en:'',
+  },
+  total_title: {
+    fi: 'Arvio Helsinki-lisästä',
+    sv: '',
+    en: '',
+  },
+  total_prefix: {
+    fi: 'Työnantajalle arvoitu Helsinki-lisä',
+    sv: '',
+    en: '',
+  },
+  total_suffix: {
+    fi: 'euroa kuukaudessa',
+    sv: '',
+    en: '',
+  },
+  total_explanation: {
+    fi: 'Arvio tuen määrästä on suuntaa antava eikä se takaa lopullista tuen määrää. Helsinki-lisää myönnetään työsuhteen ajaksi, kuitenkin enintään 12 kuukaudeksi. Tuen myöntäminen edellyttää, että työllistettävälle on myönnetty Helsinki-lisä-kortti, palkkatukea tai 55 vuotta täyttäneiden työllistämistukea.',
+    sv: '',
+    en: '',
+  },
+  breakdown_title: {
+    fi: 'Arvio muodostuu seuraavista tiedoista:',
+    sv: '',
+    en: '',
+  },
+  subtotal_title: {
+    fi: 'Työsuhde',
+    sv: '',
+    en: '',
+  },
+  subtotal_details_1: {
+    fi: 'Työllistettävän bruttopalkka: ${value} euroa kuukaudessa.',
+    sv: '',
+    en: '',
+  },
+  subtotal_details_2: {
+    fi: 'Lomaraha: ${value} euroa kuukaudessa.',
+    sv: '',
+    en: '',
+  },
+  subtotal_details_3: {
+    fi: 'Sivukulut: ${value} euroa bruttopalkasta. Arviossa on käytetty keskimääräistä arviota sivukuluista.',
+    sv: '',
+    en: '',
+  },
+  subtotal_details_4: {
+    fi: 'Työsuhteeseen on myönnetty palkkatuki, joka kattaa ${value} % palkkakustannuksista.',
+    sv: '',
+    en: '',
+  },
+  additional_details_title:{
+    fi: 'Lisähuomiot',
+    sv: '',
+    en: '',
+  },
+  additional_details_text_1: {
+    fi: 'Oppisopimuksissa Helsinki-lisää voidaan myöntää koko oppisopimuksen ajaksi.',
+    sv: '',
+    en: '',
+  },
+  additional_details_text_2: {
+    fi: 'Laskuri ei tue tapausta, jossa palkkatuen määrä on 100 prosenttia työntekijän palkkakustannuksista 65 prosentin työajalla.',
+    sv: '',
+    en: '',
+  },
+  error_calculation_title:{
+    fi: 'Laskenta epäonnistui.',
+    sv: '',
+    en: '',
+  },
+  error_calculation_message:{
+    fi: 'Ole hyvä ja tarkista syöttämäsi tiedot.',
+    sv: '',
+    en: '',
+  }
+};
+
+export default translations;
+  
+/*
+key: {
+  fi: "",
+  sv: "",
+  en: "",
+},
+*/
