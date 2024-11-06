411bb69d5513ea6d9facdae25c78475180a2cead
Jere Ljungberg <jere@mysteerio.fi>
2024-11-06 14:56:21 +0200

    Add timeout to fetches

---
+++ src/js/react/common/hooks/useTimeoutFetch.ts
@@ -0,0 +1,12 @@
+export default async function useTimeoutFetch(url: RequestInfo|string, options?: RequestInit, timeout: number = 8000) {
+  const controller = new AbortController();
+  const enforceTimeout = setTimeout(() => controller.abort(), timeout);
+
+  const result = await fetch(url, {
+    ...options,
+    signal: controller.signal
+  });
+  clearTimeout(enforceTimeout);
+
+  return result;
+};
