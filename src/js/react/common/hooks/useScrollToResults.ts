2edd11af482da0f9a9e83fbd257c15f0c95a028b
Anna Lonka <anna.lonka@druid.fi>
2025-02-05 16:34:37 +0200

    UHF-10235: Changed how scroll is called in job listing search results container

--- src/js/react/common/hooks/useScrollToResults.ts
+++ src/js/react/common/hooks/useScrollToResults.ts
@@ -5,11 +5,9 @@ const useScrollToResults = (ref: RefObject<HTMLElement>, shouldScrollOnRender: b
     const { current } = ref;
 
     if (current && shouldScrollOnRender) {
-      setTimeout(() => {
-        current.setAttribute('tabindex', '-1');
-        current.focus({ preventScroll: true });
-        current.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      }, 50); // UHF-10235 Small delay to fix screen reader focus issue on safari
+      current.setAttribute('tabindex', '-1');
+      current.focus({ preventScroll: true });
+      current.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
   }, [ref, shouldScrollOnRender]);
 };
