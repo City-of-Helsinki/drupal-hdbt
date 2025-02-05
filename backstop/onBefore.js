b7c62e33bac2621ffb1c70002b91f127b968c5f7
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-02-05 12:16:56 +0200

    UHF-11329: Skip the HDBT cookie banner element when testing scenarios.

--- backstop/onBefore.js
+++ backstop/onBefore.js
@@ -1,32 +1,6 @@
-module.exports = async (page, scenario, vp, isReference, browserContext, config) => {
+module.exports = async (page) => {
+  // Simulate the "prefers-reduced-motion: reduce" media query,
+  // which is a user setting that reduces animations and transitions.
+  // It will provide less distorted test images.
   await page.emulateMedia({ reducedMotion: 'reduce' });
-
-  const { hostname } = config;
-
-  browserContext.addCookies([
-    {
-      'name': 'cookie-agreed-version',
-      'value': '1.1.0',
-      'domain': hostname,
-      'path': '/',
-      'httpOnly': false,
-      'secure': false
-    },
-    {
-      'name': 'cookie-agreed',
-      'value': '2',
-      'domain': hostname,
-      'path': '/',
-      'httpOnly': false,
-      'secure': false
-    },
-    {
-      'name': 'cookie-agreed-categories',
-      'value': '%5B%22essential%22%2C%22preference%22%2C%22statistics%22%2C%22marketing%22%2C%22chat%22%5D',
-      'domain': hostname,
-      'path': '/',
-      'httpOnly': false,
-      'secure': false
-    }
-  ]);
 };
