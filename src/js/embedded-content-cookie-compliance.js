b138c390949dcb62889fea2d166a5f02093ba7c2
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-02-06 13:20:53 +0200

    UHF-11329: Refactored the skip links.

--- src/js/embedded-content-cookie-compliance.js
+++ src/js/embedded-content-cookie-compliance.js
@@ -21,86 +21,117 @@
           return;
         }
 
-      const mediaContainers = $(`.embedded-content-cookie-compliance.media-${id}`);
+        const mediaContainers = $(`.embedded-content-cookie-compliance.media-${id}`);
 
-      // Each of the media type is grouped to their own mediaContainers so we need to iterate through them.
-      mediaContainers.each(function processMediaContainer() {
-        const mediaContainer = $(this);
+        // Each of the media type is grouped to their own
+        // mediaContainers so we need to iterate through them.
+        mediaContainers.each(function processMediaContainer(index) {
+          const mediaContainer = $(this);
 
-        const iframeElement = document.createElement('iframe');
-        iframeElement.classList.add('media-oembed-content');
-        iframeElement.src = attributes.src;
-        iframeElement.title = attributes.title;
+          const iframeElement = document.createElement('iframe');
+          iframeElement.classList.add('media-oembed-content');
+          iframeElement.src = attributes.src;
+          iframeElement.title = attributes.title;
 
-        if (attributes.allow) {
-          iframeElement.allow = attributes.allow;
-        }
+          if (attributes.allow) {
+            iframeElement.allow = attributes.allow;
+          }
 
-        if (attributes.height) {
-          iframeElement.height = attributes.height;
-        }
-        if (attributes.width) {
-          iframeElement.width = attributes.width;
-        }
+          if (attributes.height) {
+            iframeElement.height = attributes.height;
+          }
+          if (attributes.width) {
+            iframeElement.width = attributes.width;
+          }
 
-        const skipLinkBefore = document.createElement('a');
-        skipLinkBefore.classList.add('focusable', 'skip-link');
-        skipLinkBefore.href = `#${attributes.skipLinkAfterId}`;
-        skipLinkBefore.id = attributes.skipLinkBeforeId;
+          const containerElement = document.createElement('div');
+          containerElement.appendChild(iframeElement);
 
-        const skipLinkAfter = document.createElement('a');
-        skipLinkAfter.classList.add('focusable', 'skip-link');
-        skipLinkAfter.href = `#${attributes.skipLinkBeforeId}`;
-        skipLinkAfter.id = attributes.skipLinkAfterId;
+          // Extract the media name from the wrapping component element or in
+          // case of video, from the remote video - video title.
+          // Fallback to empty if no title is set.
+          let mediaName = '';
+          switch (attributes.type) {
+            case 'video':
+              mediaName = mediaContainer.parent().siblings('.remote-video__video-title').text().trim() || '';
+              break;
+            default:
+              mediaName = mediaContainer.parent().prevAll('h2').first().text().trim() || '';
+              break;
+          }
 
-        const containerElement = document.createElement('div');
-        containerElement.appendChild(iframeElement);
+          // Initialize skip links elements.
+          const skipLinkBefore = document.createElement('a');
+          const skipLinkAfter = document.createElement('a');
 
-        // Extract the media name from the wrapping component element.
-        // Fallback to empty if no title is set. Currently only used in map.
-        const mediaName = mediaContainer.parent().prevAll('h2').first().text().trim() || '';
+          // Construct skip links if their IDs are set.
+          if (attributes.skipLinkBeforeId && attributes.skipLinkAfterId) {
+            skipLinkBefore.classList.add('focusable', 'skip-link');
+            skipLinkBefore.href = `#${attributes.skipLinkAfterId}-${index}`;
+            skipLinkBefore.id = `${attributes.skipLinkBeforeId}-${index}`;
 
-        switch (attributes.type) {
-          case 'video':
-          case 'chart':
-            containerElement.classList.add(`responsive-${attributes.type}-container`);
-            mediaContainer
-              .empty()
-              .append(containerElement)
-              .removeClass(`media-${id}`);
-            break;
+            skipLinkAfter.classList.add('focusable', 'skip-link');
+            skipLinkAfter.href = `#${attributes.skipLinkBeforeId}-${index}`;
+            skipLinkAfter.id = `${attributes.skipLinkAfterId}-${index}`;
+          }
 
-          case 'journey_planner':
-            containerElement.classList.add('journey-planner-container');
-            skipLinkAfter.classList.add('skip-link--planner--after');
-            skipLinkBefore.classList.add('skip-link--planner--before');
-            skipLinkAfter.text = Drupal.t('Continue above the journey planner', {}, { context: 'Skip link after the journey planner for the journey planner paragraph' });
-            skipLinkBefore.text = Drupal.t('Continue below the journey planner', {}, { context: 'Skip link before the journey planner for the journey planner paragraph' });
-            mediaContainer
-              .replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
-            break;
+          switch (attributes.type) {
+            case 'video':
+              containerElement.classList.add(`responsive-${attributes.type}-container`);
+              skipLinkAfter.classList.add('skip-link--video--after');
+              skipLinkBefore.classList.add('skip-link--video--before');
 
-          case 'map':
-            containerElement.classList.add('responsive-map-container');
-            skipLinkAfter.classList.add('skip-link--map--after');
-            skipLinkBefore.classList.add('skip-link--map--before');
+              // Adjust the skip link text based on whether the mediaName is found.
+              skipLinkAfter.text = Drupal.t('Continue above the @video video', { '@video': mediaName }, { context: 'Skip links' });
+              skipLinkBefore.text = Drupal.t('Continue below the @video video', { '@video': mediaName }, { context: 'Skip links' });
+              mediaContainer.replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
+              break;
 
-            // Adjust the skip link text based on whether the mediaName is found.
-            skipLinkAfter.text = mediaName
-              ? Drupal.t('Continue above the @map map', { '@map': mediaName }, { context: 'Skip link after the map for the map paragraph' })
-              : Drupal.t('Continue above the map', {}, { context: 'Skip link after the map for the map paragraph' });
+            case 'chart':
+              containerElement.classList.add(`responsive-${attributes.type}-container`);
+              skipLinkAfter.classList.add('skip-link--chart--after');
+              skipLinkBefore.classList.add('skip-link--chart--before');
 
-            skipLinkBefore.text = mediaName
-              ? Drupal.t('Continue below the @map map', { '@map': mediaName }, { context: 'Skip link before the map for the map paragraph' })
-              : Drupal.t('Continue below the map', {}, { context: 'Skip link before the map for the map paragraph' });
+              // Adjust the skip link text based on whether the mediaName is found.
+              skipLinkAfter.text = mediaName
+                ? Drupal.t('Continue above the @chart chart', { '@chart': mediaName }, { context: 'Skip links' })
+                : Drupal.t('Continue above the chart', {}, { context: 'Skip links' });
+              skipLinkBefore.text = mediaName
+                ? Drupal.t('Continue below the @chart chart', { '@chart': mediaName }, { context: 'Skip links' })
+                : Drupal.t('Continue below the chart', {}, { context: 'Skip links' });
 
-            mediaContainer.replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
-            break;
+              mediaContainer.replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
+              break;
 
-          default:
-            break;
-        }
-      });
+            case 'journey_planner':
+              containerElement.classList.add('journey-planner-container');
+              skipLinkAfter.classList.add('skip-link--planner--after');
+              skipLinkBefore.classList.add('skip-link--planner--before');
+              skipLinkAfter.text = Drupal.t('Continue above the journey planner', {}, { context: 'Skip links' });
+              skipLinkBefore.text = Drupal.t('Continue below the journey planner', {}, { context: 'Skip links' });
+              mediaContainer.replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
+              break;
+
+            case 'map':
+              containerElement.classList.add('responsive-map-container');
+              skipLinkAfter.classList.add('skip-link--map--after');
+              skipLinkBefore.classList.add('skip-link--map--before');
+
+              // Adjust the skip link text based on whether the mediaName is found.
+              skipLinkAfter.text = mediaName
+                ? Drupal.t('Continue above the @map map', { '@map': mediaName }, { context: 'Skip links' })
+                : Drupal.t('Continue above the map', {}, { context: 'Skip links' });
+              skipLinkBefore.text = mediaName
+                ? Drupal.t('Continue below the @map map', { '@map': mediaName }, { context: 'Skip links' })
+                : Drupal.t('Continue below the map', {}, { context: 'Skip links' });
+
+              mediaContainer.replaceWith(skipLinkBefore, containerElement, skipLinkAfter);
+              break;
+
+            default:
+              break;
+          }
+        });
     });
   };
 
