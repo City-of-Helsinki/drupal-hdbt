98beda78f293ba5aa6a16f84a019335d239f8c90
Tero Elonen <tero.elonen@druid.fi>
2025-03-21 20:40:46 +0200

    Clean up the language toast javascript a bit

--- src/js/language-toast.js
+++ src/js/language-toast.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function languageToastPositioning() {
   const buttons = document.querySelectorAll('.nav-toggle__button.has-toast');
 
   function positionToast(button, toast) {
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if ((viewportWidth - rect.right) < ((toastWidth / 2) - (buttonWidth / 2) + gutter)) {
       const right = viewportWidth - rect.right - gutter;
       const toastArrowRight = ((viewportWidth - rect.right) - gutter) + (buttonWidth / 2);
+
       toast.style.left = 'auto';
       toast.style.transform = 'none';
       toast.style.right = `-${right}px`;
@@ -29,22 +30,24 @@ document.addEventListener('DOMContentLoaded', function () {
     if ((rect.left) < ((toastWidth / 2) - (buttonWidth / 2) + gutter)) {
       const left = rect.left - gutter;
       const toastArrowLeft = (rect.left - gutter) + (buttonWidth / 2);
+
       toast.style.left = `-${left}px`;
       toast.style.transform = 'none';
       toast.style.setProperty('--toast-arrow-left', `${toastArrowLeft}px`);
     }
   }
 
+  // Handle click event and position the toast correctly before opening it.
   buttons.forEach(button => {
-    button.addEventListener('click', function () {
-      const buttonParent = button.parentElement; // Get parent of the button
+    button.addEventListener('click', function languageToastClick() {
+      const buttonParent = button.parentElement;
       const toast = buttonParent.nextElementSibling;
 
       if (!toast || !toast.classList.contains('nav-toggle-dropdown--language-toast')) {
         return;
       }
 
-      // Reset inline styles before applying new ones
+      // Reset inline styles before applying new ones.
       toast.removeAttribute('style');
 
       // Position and after that show the toast.
@@ -53,10 +56,11 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Handle resize event to reposition open toasts.
-  window.addEventListener('resize', function () {
+  window.addEventListener('resize', function languageToastResize() {
     document.querySelectorAll('.nav-toggle__button.has-toast').forEach(button => {
-      const buttonParent = button.parentElement; // Get parent of the button
+      const buttonParent = button.parentElement;
       const toast = buttonParent.nextElementSibling;
+
       if (toast && !toast.classList.contains('nav-toggle-dropdown--closed')) {
         toast.removeAttribute('style');
         positionToast(button, toast);
