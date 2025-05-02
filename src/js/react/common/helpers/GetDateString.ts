9c9860602cc8cd5911147f7c0e2b8bbe74396802
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-05-02 14:27:49 +0300

    UHF-11374: Improving screen reader compatibility on date range selector.

--- src/js/react/common/helpers/GetDateString.ts
+++ src/js/react/common/helpers/GetDateString.ts
@@ -1,18 +1,29 @@
 import type DateSelectDateTimes from '@/types/DateSelectDateTimes';
 import { HDS_DATE_FORMAT } from '../enum/HDSDateFormat';
 
-export const getDateString = ({ startDate, endDate }: DateSelectDateTimes): string => {
+export const getDateString = ({ startDate, endDate, showLabels }: DateSelectDateTimes): string => {
   if ((!startDate || !startDate.isValid) && (!endDate || !endDate.isValid)) {
     return Drupal.t('All dates', {}, { context: 'Events search' });
   }
 
   if ((startDate && startDate.isValid) && (!endDate || !endDate.isValid)) {
+    if (showLabels) {
+      return Drupal.t('From @date', { '@date': startDate.toFormat(HDS_DATE_FORMAT) }, { context: 'Events search' });
+    }
     return startDate.toFormat(HDS_DATE_FORMAT);
   }
 
   if ((!startDate || !startDate.isValid) && endDate?.isValid) {
+    if (showLabels) {
+      return Drupal.t('Until @date', { '@date': endDate.toFormat(HDS_DATE_FORMAT) }, { context: 'Events search' });
+    }
     return `- ${endDate.toFormat(HDS_DATE_FORMAT)}`;
   }
+
+  if (showLabels) {
+    return Drupal.t('From @date until @date2', { '@date': startDate?.toFormat(HDS_DATE_FORMAT), '@date2': endDate?.toFormat(HDS_DATE_FORMAT) }, { context: 'Events search' });
+  }
+
   return `${startDate?.toFormat(HDS_DATE_FORMAT) || 'unset?'} - ${endDate?.toFormat(HDS_DATE_FORMAT)}`;
 };
 
