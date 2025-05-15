3c9235979c6f4edb7ea7ab9979169ea5ceafb2e8
Anna Lonka <anna.lonka@druid.fi>
2025-05-15 13:33:17 +0300

    UHF-11870: Fixed lable issue with job search

--- src/js/react/apps/job-search/store.tsx
+++ src/js/react/apps/job-search/store.tsx
@@ -234,7 +234,7 @@ export const employmentAtom = atom(async(get) => {
       const customId = term._source.field_search_id?.[0];
       let count = 0;
       let additionalValue = null;
-      let label = `${term._source.name} (${count})`;
+      let label = '';
       let simpleLabel = term._source.name;
 
       if (!customId) {
@@ -261,7 +261,10 @@ export const employmentAtom = atom(async(get) => {
         simpleLabel = Drupal.t('Fixed-term contract and service employment', {}, { context: 'Employment filter selection value'});
       }
       else {
+        console.log(combinedAggs);
         count = combinedAggs.get(tid) || 0;
+        console.log(count);
+        label = `${term._source.name} (${count})`;
       }
 
 
