b7f4782d7035f1bfc62776871bf0170dd6127311
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-05-23 12:56:49 +0300

    UHF-11833: Refactored sidebar-menu-toggle.js to use drupal behaviors.

--- src/js/sidebar-menu-toggle.js
+++ src/js/sidebar-menu-toggle.js
@@ -1,39 +1,43 @@
-document.addEventListener('DOMContentLoaded', function startSidebarNavigation() {
-  function toggleSidebarMenuVisibility(item, cssClass) {
-    const toggleButton = item.querySelector('.menu__toggle-button');
+const toggleSidebarMenuVisibility = (item, cssClass) => {
+  const toggleButton = item.querySelector('.menu__toggle-button');
 
-    // Check if there was menu toggle button under the menu item.
-    if (toggleButton !== null) {
-      toggleButton.addEventListener('click', function onClickToggle(event) {
-        item.classList.toggle(cssClass);
-        toggleButton.setAttribute(
-          'aria-expanded',
-          toggleButton.getAttribute('aria-expanded') === 'true' ? 'false' : 'true',
-        );
-        event.stopPropagation();
-      });
-    }
+  // Check if there was menu toggle button under the menu item.
+  if (toggleButton !== null) {
+    toggleButton.addEventListener('click', (event) => {
+      item.classList.toggle(cssClass);
+      toggleButton.setAttribute(
+        'aria-expanded',
+        toggleButton.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+      );
+      event.stopPropagation();
+    });
   }
+};
 
-  // Find all menu items with children menus.
-  const sidebarNavigation = document.getElementsByClassName('sidebar-navigation');
-  if (typeof sidebarNavigation !== 'undefined') {
-    if (sidebarNavigation[0]) {
-      const itemsWithChildren = sidebarNavigation[0].getElementsByClassName('menu__item--children');
-
-      // eslint-disable-next-line no-restricted-syntax
-      for (const item of itemsWithChildren) {
-        toggleSidebarMenuVisibility(item, 'menu__item--open');
+((Drupal) => {
+  Drupal.behaviors.toggleSidebarNavigation = {
+    attach(context) {
+      if (context !== document || window.sidebarNavigationInitialized) {
+        return;
       }
-    }
-  }
 
-  // In case of section navigation find the section navigation.
-  const sectionNavigation = document.getElementsByClassName('sidebar-navigation--section-navigation');
-  if (typeof sectionNavigation !== 'undefined') {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const item of sectionNavigation) {
-      toggleSidebarMenuVisibility(item, 'sidebar-navigation--section-navigation--open');
+      // Find all menu items with children menus.
+      const sidebarNavigation = context.querySelector('.sidebar-navigation');
+
+      if (sidebarNavigation) {
+        const itemsWithChildren = sidebarNavigation.querySelectorAll('.menu__item--children');
+        itemsWithChildren.forEach((item) => {
+          toggleSidebarMenuVisibility(item, 'menu__item--open');
+        });
+      }
+
+      // In case of section navigation find the section navigation.
+      const sectionNavigations = context.querySelectorAll('.sidebar-navigation--section-navigation');
+      sectionNavigations.forEach((item) => {
+        toggleSidebarMenuVisibility(item, 'sidebar-navigation--section-navigation--open');
+      });
+
+      window.sidebarNavigationInitialized = true;
     }
-  }
-});
+  };
+})(Drupal);
