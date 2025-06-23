666f5c9105507a6c1b681f07ca21068adb145844
Jere Ljungberg <jere@mysteerio.fi>
2025-06-23 13:39:21 +0300

    Fix clarity issues with school search

--- src/js/react/apps/school-search/containers/ProximityFormContainer.tsx
+++ src/js/react/apps/school-search/containers/ProximityFormContainer.tsx
@@ -1,23 +1,22 @@
-import { Button, TextInput } from 'hds-react';
-import { useSetAtom } from 'jotai';
+import { Button } from 'hds-react';
+import { createRef } from 'react';
+import { useAtom, useSetAtom } from 'jotai';
 
-import { paramsAtom } from '../store';
+import { AddressSearch } from '@/react/common/AddressSearch';
+import { keywordAtom, paramsAtom } from '../store';
 import SearchParams from '../types/SearchParams';
 
-type SubmitFormType = HTMLFormElement & {
-  keyword: HTMLInputElement;
-};
-
 const ProximityFormContainer = ({ initialAddress }: { initialAddress?: string}) => {
+  const formRef = createRef<HTMLFormElement>();
   const setParams = useSetAtom(paramsAtom);
+  const [keyword, setKeyword] = useAtom(keywordAtom);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const { keyword } = event.target as SubmitFormType;
     const params: SearchParams = {};
 
-    if (keyword.value && keyword.value.length) {
-      params.keyword = keyword.value;
+    if (keyword && keyword.length) {
+      params.keyword = keyword;
     }
 
     setParams(params);
@@ -26,7 +25,12 @@ const ProximityFormContainer = ({ initialAddress }: { initialAddress?: string}) 
   };
 
   return (
-    <form className='hdbt-search--react__form-container' role='search' onSubmit={onSubmit}>
+    <form
+      className='hdbt-search--react__form-container'
+      onSubmit={onSubmit}
+      ref={formRef}
+      role='search'
+    >
       <h3>
         {Drupal.t('Search for your local school', {}, {context: 'School search: local search title'})}
       </h3>
@@ -37,14 +41,20 @@ const ProximityFormContainer = ({ initialAddress }: { initialAddress?: string}) 
           {context: 'School search: local search description'}
         )}
       </p>
-      <TextInput
+      <AddressSearch
         className='hdbt-search__filter'
+        clearButtonAriaLabel={Drupal.t('Clear', {}, { context: 'React search'})}
+        defaultValue={initialAddress || ''}
         helperText={Drupal.t('Enter the street name and house number', {}, { context: 'React search: street input helper'})}
         id='keyword'
         label={Drupal.t('The child\'s home address', {}, { context: 'School search: input label'})}
-        type='search'
-        defaultValue={initialAddress || ''}
-        clearButtonAriaLabel={Drupal.t('Clear', {}, { context: 'React search'})}
+        onChange={(value: string) => setKeyword(value)}
+        onSubmit={(value: string) => {
+          setKeyword(value);
+          formRef.current?.requestSubmit();
+        }}
+        visibleSuggestions={5}
+        value={keyword}
       />
       <Button className='hdbt-search--react__submit-button' type='submit'>{Drupal.t('Search', {}, { context: 'React search: submit button label'})}</Button>
     </form>
