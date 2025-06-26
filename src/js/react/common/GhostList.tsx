40eea6f41028f360dd18e61c69cab44cc2b2084e
Anna Lonka <anna.lonka@druid.fi>
2025-06-26 15:35:31 +0300

    UHF-11369: Changed aria live to assertive

--- src/js/react/common/GhostList.tsx
+++ src/js/react/common/GhostList.tsx
@@ -7,7 +7,7 @@ export const GhostList = ({
   bordered?: boolean,
   count: number,
 }) => (
-  <div aria-live="polite">
+  <div aria-live="assertive">
     <div className="visually-hidden">{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
     {Array.from(Array(count)).map((_, i) => <CardGhost bordered={bordered} key={i} />)}
   </div>
--- src/js/react/common/LoadingOverlay.tsx
+++ src/js/react/common/LoadingOverlay.tsx
@@ -1,7 +1,7 @@
 import { LoadingSpinner } from 'hds-react';
 
 const LoadingOverlay = () => (
-  <div className='hdbt__loading-overlay' aria-live='polite'>
+  <div className='hdbt__loading-overlay' aria-live='assertive'>
     <div className="visually-hidden">{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
     <LoadingSpinner loadingText="" loadingFinishedText="" />
   </div>
