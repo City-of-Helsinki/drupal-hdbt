5f5908ed9e197c2e150d5f9fea9531b3761369d7
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2025-07-01 13:48:15 +0300

    UHF-11890: cleanup, try to use proper hds components...

--- src/js/react/apps/roadworks/components/RoadworkCard.tsx
+++ src/js/react/apps/roadworks/components/RoadworkCard.tsx
@@ -1,61 +1,29 @@
 import React from 'react';
-import { Card, IconClock, IconLocation, IconLinkExternal, IconCogwheel } from 'hds-react';
+import { IconClock, IconLocation, IconCogwheel } from 'hds-react';
 
 import type Roadwork from '../types/Roadwork';
+import CardItem from '@/react/common/Card';
 
 interface RoadworkCardProps {
   roadwork: Roadwork;
+  cardModifierClass?: string;
 }
 
-const RoadworkCard: React.FC<RoadworkCardProps> = ({ roadwork }) => {
+const RoadworkCard: React.FC<RoadworkCardProps> = ({ roadwork, cardModifierClass }) => {
   const { title, description, url, location, schedule } = roadwork;
 
   return (
-    <Card>
-      <div className="roadwork-card">
-        <div className="roadwork-card__header">
-          <IconCogwheel aria-hidden="true" />
-          <h3 className="roadwork-card__title">
-            {url ? (
-              <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hds-link hds-link--external"
-              >
-                {title}
-                <IconLinkExternal aria-hidden="true" />
-                <span className="visually-hidden"> (avautuu uudessa välilehdessä)</span>
-              </a>
-            ) : (
-              title
-            )}
-          </h3>
-        </div>
-
-        {description && (
-          <div className="roadwork-card__description">
-            <p>{description}</p>
-          </div>
-        )}
-
-        <div className="roadwork-card__details">
-          {location && (
-            <div className="roadwork-card__detail">
-              <IconLocation aria-hidden="true" />
-              <span>{location}</span>
-            </div>
-          )}
-          
-          {schedule && (
-            <div className="roadwork-card__detail">
-              <IconClock aria-hidden="true" />
-              <span>{schedule}</span>
-            </div>
-          )}
-        </div>
-      </div>
-    </Card>
+    <CardItem
+      cardTitle={title}
+      cardUrl={url || '#'}
+      cardUrlExternal={!!url}
+      cardDescription={description}
+      cardModifierClass={cardModifierClass}
+      location={location}
+      locationLabel="Sijainti"
+      time={schedule}
+      timeLabel="Aikataulu"
+    />
   );
 };
 
