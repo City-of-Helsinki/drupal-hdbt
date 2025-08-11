a1a3f1cf5a7e92471f0d7b10e21dd1af718be1a2
Tero Elonen <tero.elonen@druid.fi>
2025-08-11 12:03:20 +0300

    Add helper function to get the current language when needed

---
+++ src/js/react/common/helpers/GetCurrentLanguage.ts
@@ -0,0 +1,8 @@
+// helpers/getCurrentLanguage.ts
+export type SupportedLanguage = 'fi' | 'sv' | 'en';
+
+export function getCurrentLanguage(
+  lang: string | undefined
+): SupportedLanguage {
+  return lang === 'fi' || lang === 'sv' || lang === 'en' ? lang : 'fi';
+}
