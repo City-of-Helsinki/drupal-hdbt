868f19a2e2031dde24c11b6acc82c2082e10fd45
Tero Elonen <tero.elonen@druid.fi>
2025-09-10 16:59:30 +0300

    UHF-12142: Add aria-atomic attribute to React LoadingOverlay and GhostList components

--- src/js/react/common/GhostList.tsx
+++ src/js/react/common/GhostList.tsx
@@ -7,8 +7,8 @@ export const GhostList = ({
   bordered?: boolean,
   count: number,
 }) => (
-  <div aria-live="assertive">
-    <div className="visually-hidden">{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
+  <div aria-live='assertive' aria-atomic='true'>
+    <div className='visually-hidden'>{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
     {Array.from(Array(count)).map((_, i) => <CardGhost bordered={bordered} key={i} />)}
   </div>
 );
--- src/js/react/common/LoadingOverlay.tsx
+++ src/js/react/common/LoadingOverlay.tsx
@@ -1,9 +1,9 @@
 import { LoadingSpinner } from 'hds-react';
 
 const LoadingOverlay = () => (
-  <div className='hdbt__loading-overlay' aria-live='assertive'>
-    <div className="visually-hidden">{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
-    <LoadingSpinner loadingText="" loadingFinishedText="" />
+  <div className='hdbt__loading-overlay' aria-live='assertive'  aria-atomic='true'>
+    <div className='visually-hidden'>{Drupal.t('Search results are loading', {}, {context: 'React search: results loading'})}</div>
+    <LoadingSpinner loadingText='' loadingFinishedText='' />
   </div>
 );
 
