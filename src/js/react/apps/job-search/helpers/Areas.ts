7c88c6c634c5d183115902943d4695fed0bfae17
Anna Lonka <anna.lonka@druid.fi>
2025-11-12 09:05:01 +0200

    Added type for areainfo object

--- src/js/react/apps/job-search/helpers/Areas.ts
+++ src/js/react/apps/job-search/helpers/Areas.ts
@@ -3,40 +3,46 @@ import OptionType from '../types/OptionType';
 
 export const getInitialAreaFilter = (key: string[] | string = '', options: OptionType[] = []) => options.find((option: OptionType) => option?.value === key.toString()) || null;
 
-export const getAreaInfo = [ 
+interface AreaInfo {
+  key: keyof typeof PostalCodes;
+  label: string;
+  postalCodes: string[];
+}
+
+export const getAreaInfo: AreaInfo[] = [
   {    
     key: 'eastern',
-    label: Drupal.t('Eastern area', {}, {context: 'Search filter option: Eastern area'}),
+    label: Drupal.t('Eastern area', {}, { context: 'Search filter option: Eastern area' }),
     postalCodes: PostalCodes.eastern,
   },
   {
     key: 'central',
-    label: Drupal.t('Central area', {}, {context: 'Search filter option: Central area'}),
+    label: Drupal.t('Central area', {}, { context: 'Search filter option: Central area' }),
     postalCodes: PostalCodes.central,    
   },
   { 
-    key: 'southern',   
-    label: Drupal.t('Southern area', {}, {context: 'Search filter option: Southern area'}),
+    key: 'southern',
+    label: Drupal.t('Southern area', {}, { context: 'Search filter option: Southern area' }),
     postalCodes: PostalCodes.southern,
   },
   { 
-    key: 'southeastern',   
-    label: Drupal.t('South-Eastern area', {}, {context: 'Search filter option: South-Eastern area'}),
+    key: 'southeastern',
+    label: Drupal.t('South-Eastern area', {}, { context: 'Search filter option: South-Eastern area' }),
     postalCodes: PostalCodes.southeastern,    
   },
   { 
-    key: 'western',   
-    label: Drupal.t('Western area', {}, {context: 'Search filter option: Western area'}),
+    key: 'western',
+    label: Drupal.t('Western area', {}, { context: 'Search filter option: Western area' }),
     postalCodes: PostalCodes.western,
   },
   {
     key: 'northern',
-    label: Drupal.t('Northern area', {}, {context: 'Search filter option: Northern area'}),
+    label: Drupal.t('Northern area', {}, { context: 'Search filter option: Northern area' }),
     postalCodes: PostalCodes.northern,
-  },  
+  },
   { 
-    key: 'northeast',   
-    label: Drupal.t('North-Eastern area', {}, {context: 'Search filter option: North-Eastern area'}),
+    key: 'northeast',
+    label: Drupal.t('North-Eastern area', {}, { context: 'Search filter option: North-Eastern area' }),
     postalCodes: PostalCodes.northeast,
   },
 ];
