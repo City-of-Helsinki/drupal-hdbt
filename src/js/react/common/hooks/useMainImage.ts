50c169cf6433ee81432e91aea90c901a5ad348a5
Jere Ljungberg <jere@mysteerio.fi>
2026-04-02 15:07:02 +0300

    UHF-13100: Add safeguards to main image hook

--- src/js/react/common/hooks/useMainImage.ts
+++ src/js/react/common/hooks/useMainImage.ts
@@ -1,24 +1,31 @@
+const defaultResult = {
+  src: '',
+  keyedStyles: {},
+};
+
 export const useMainImage = (main_image_urls?: string[]) => {
   if (!main_image_urls?.length) {
-    return {
-      src: '',
-      keyedStyles: {},
-    };
+    return defaultResult;
   }
 
-  const { original, styles } = main_image_urls?.length ? JSON.parse(main_image_urls[0]) : {};
+  try {
+    const { original, styles } = main_image_urls?.length ? JSON.parse(main_image_urls[0]) : {};
 
-  const keyedStyles = Object.keys(styles || {}).reduce(
-    (acc, key) => {
-      const { breakpoint, url } = styles[key];
-      acc[breakpoint] = url;
-      return acc;
-    },
-    {} as Record<string, string>,
-  );
+    const keyedStyles = Object.keys(styles || {}).reduce(
+      (acc, key) => {
+        const { breakpoint, url } = styles[key];
+        acc[breakpoint] = url;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
 
-  return {
-    src: original?.url || styles?.['1248'] || '',
-    keyedStyles,
-  };
+    return {
+      src: original?.url || keyedStyles?.['1248'] || '',
+      keyedStyles,
+    };
+  } catch (error) {
+    console.error('Error parsing main image URLs:', error);
+    return defaultResult;
+  }
 };
