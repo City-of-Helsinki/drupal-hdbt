dccd69db563da547a007c88919d3e8879c2debec
Tero Elonen <tero.elonen@druid.fi>
2026-04-14 12:52:15 +0300

    Add more modification possibilities for the Ghost cards

--- src/js/react/common/CardGhost.tsx
+++ src/js/react/common/CardGhost.tsx
@@ -1,6 +1,6 @@
-export const CardGhost = ({ bordered = false }: { bordered?: boolean }) => (
-  <div className={`card card--ghost${bordered ? ' card--border' : ''}`}>
-    <div className='card__image'></div>
+export const CardGhost = ({ bordered = false, simple = false }: { bordered?: boolean; simple?: boolean }) => (
+  <div className={`card card--ghost${simple ? ' card--ghost--simple' : ''}${bordered ? ' card--border' : ''}`}>
+    {!simple && <div className='card__image'></div>}
     <div className='card__text'>
       <div className='card__title'></div>
       <div className='card__description'></div>
--- src/js/react/common/GhostList.tsx
+++ src/js/react/common/GhostList.tsx
@@ -1,13 +1,23 @@
 import { CardGhost } from './CardGhost';
 
-export const GhostList = ({ bordered = false, count }: { bordered?: boolean; count: number }) => (
-  <div aria-live='assertive' aria-atomic='true'>
+export const GhostList = ({
+  bordered = false,
+  simple = false,
+  count,
+  modifierClass,
+}: {
+  bordered?: boolean;
+  simple?: boolean;
+  count: number;
+  modifierClass?: string;
+}) => (
+  <div aria-live='assertive' aria-atomic='true' className={modifierClass ?? undefined}>
     <div className='visually-hidden'>
       {Drupal.t('Search results are loading', {}, { context: 'React search: results loading' })}
     </div>
     {Array.from(Array(count)).map((_, i) => (
       // biome-ignore lint/suspicious/noArrayIndexKey: @todo UHF-12501
-      <CardGhost bordered={bordered} key={i} />
+      <CardGhost bordered={bordered} simple={simple} key={i} />
     ))}
   </div>
 );
