6fc0f6066875bc0163e225a2f3196173b96cca75
Jere Ljungberg <jere@mysteerio.fi>
2026-06-16 13:44:52 +0300

    UHF-13093: Expand Event.ts type definition

--- src/js/react/apps/linkedevents/types/Event.ts
+++ src/js/react/apps/linkedevents/types/Event.ts
@@ -12,21 +12,63 @@ export type EventKeyword = IncludableResource & {
 
 type EventOffers = { info_url: MultilingualString; is_free: boolean };
 
-type EventLocation = { id: string; name?: MultilingualString; street_address?: MultilingualString };
+type EventLocation = {
+  data_source?: string;
+  has_user_editable_resources?: boolean;
+  id: string;
+  name?: MultilingualString;
+  street_address?: MultilingualString;
+};
+
+export type EventExternalLink = { language?: string; link: string; name: string };
+
+type EventStatus = 'EventScheduled' | 'EventCancelled' | 'EventPostponed' | 'EventRescheduled';
 
 export type Event = {
+  '@context'?: string;
+  '@id'?: string;
+  '@type'?: string;
+  audience?: IncludableResource[];
+  audience_max_age: number | null;
+  audience_min_age: number | null;
+  created_time?: string;
+  custom_data: unknown | null;
+  data_source?: string;
+  date_published: string | null;
+  deleted?: boolean;
+  description?: MultilingualString;
   end_time: number;
   enrolment_end_time: number;
   enrolment_start_time: number;
+  environment?: string | null;
+  environmental_certificate?: string | null;
+  event_status?: EventStatus;
+  external_links?: EventExternalLink[];
+  has_user_editable_resources?: boolean;
   id: string;
-  in_language: IncludableResource[];
   images?: EventImage[];
+  info_url?: MultilingualString;
+  in_language: IncludableResource[];
   keywords?: EventKeyword[];
+  last_modified_time?: string;
+  local?: boolean;
   location?: EventLocation;
+  location_extra_info?: MultilingualString;
+  maximum_attendee_capacity: number | null;
+  minimum_attendee_capacity: number | null;
   name: MultilingualString;
   offers?: EventOffers[];
+  provider?: MultilingualString;
+  provider_contact_info?: MultilingualString | null;
+  publisher?: string;
+  registration: unknown | null;
+  replaced_by: string | null;
+  short_description?: MultilingualString;
   start_time: number;
   street_address?: MultilingualString;
+  sub_events?: IncludableResource[];
+  super_event: IncludableResource | null;
+  super_event_type: 'recurring' | 'umbrella' | null;
   type_id: 'Course' | 'General' | 'Volunteering';
 };
 
