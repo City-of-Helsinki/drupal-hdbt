9ab08faa0f08af162be587b02b9378faf9e57bd3
Jere Ljungberg <jere@mysteerio.fi>
2026-07-16 11:36:08 +0300

    UHF-12385: Fix references to old hdbt black color

--- src/js/react/common/constants/searchInputStyle.ts
+++ src/js/react/common/constants/searchInputStyle.ts
@@ -1,6 +1,6 @@
 export const defaultSearchInputTheme = {
   '--focus-outline-color': 'var(--color-coat-of-arms)',
   '--text-color-focus-outline': 'var(--color-coat-of-arms)',
-  '--menu-item-background-color-hover': 'var(--hdbt-color-black)',
+  '--menu-item-background-color-hover': 'var(--color-black-90)',
   '--menu-item-color-hover': 'var(--color-white)',
 };
