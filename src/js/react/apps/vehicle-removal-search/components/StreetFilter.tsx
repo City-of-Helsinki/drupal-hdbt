8c94b7b758219e1a9d939807159249613f6323f2
Jere Ljungberg <jere@mysteerio.fi>
2026-08-24 15:07:55 +0300

    UHF-13400: Fix issue where url param caused stale state in HDS search component

--- src/js/react/apps/vehicle-removal-search/components/StreetFilter.tsx
+++ src/js/react/apps/vehicle-removal-search/components/StreetFilter.tsx
@@ -2,7 +2,7 @@ import type { SelectProps } from 'hds-react';
 import { type Option, type SearchFunction, Select, useSelectStorage } from 'hds-react';
 import { useSetAtom } from 'jotai';
 import { useAtomCallback } from 'jotai/utils';
-import { type ReactElement, useCallback, useEffect } from 'react';
+import { type ReactElement, useCallback, useEffect, useState } from 'react';
 import { defaultMultiSelectTheme } from '@/react/common/constants/selectTheme';
 import { clearAllSelectionsFromStorage, updateSelectionsInStorage } from '@/react/common/helpers/HDS';
 import { Events } from '../enum/Event';
@@ -72,6 +72,7 @@ export const StreetFilter = () => {
   };
 
   const id = 'streets';
+  const [storageKey, setStorageKey] = useState(0);
 
   const selectStorage = useSelectStorage({
     disabled: false,
@@ -82,12 +83,15 @@ export const StreetFilter = () => {
     onChange,
     onSearch,
     open: false,
+    updateKey: String(storageKey),
     options: getStreetsValue()?.map((dm) => ({ ...dm, selected: true })) ?? [],
     clearable: true,
   });
 
   const clearAllSelections = () => {
     clearAllSelectionsFromStorage(selectStorage);
+    // Force reload to prevent stale internal state.
+    setStorageKey((key) => key + 1);
   };
 
   const updateSelections = () => {
