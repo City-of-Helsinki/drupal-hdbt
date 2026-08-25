e07ada87cfeed11bb93083d412880b5849cce38f
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-08-25 09:07:28 +0300

    UHF-13315: Add linked events keyword sets

--- src/js/react/common/enum/LinkedEvents.ts
+++ src/js/react/common/enum/LinkedEvents.ts
@@ -1,6 +1,10 @@
 const LinkedEvents = {
   PLACES_URL: 'https://api.hel.fi/linkedevents/v1/place/',
   KEYWORDS_URL: 'https://api.hel.fi/linkedevents/v1/keyword/',
+  MAIN_CATEGORY_URLS: {
+    General: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:topics/?include=keywords',
+    Course: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:courses/?include=keywords',
+  },
 };
 
 export default LinkedEvents;
