fe3c63aa8e42eb9692b213ae951d85f43e3b3b52
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-08-25 09:07:31 +0300

    UHF-13315: Update URLs

--- src/js/react/common/enum/LinkedEvents.ts
+++ src/js/react/common/enum/LinkedEvents.ts
@@ -2,8 +2,8 @@ const LinkedEvents = {
   PLACES_URL: 'https://api.hel.fi/linkedevents/v1/place/',
   KEYWORDS_URL: 'https://api.hel.fi/linkedevents/v1/keyword/',
   MAIN_CATEGORY_URLS: {
-    General: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:topics/?include=keywords',
-    Course: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:courses/?include=keywords',
+    General: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:af44xv4uju/?include=keywords',
+    Course: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:af44xwswri/?include=keywords',
   },
 };
 
