866b610df7ccc7195a04145d4a081c0d5a1c1c58
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-08-25 09:07:31 +0300

    UHF-13315: Add hook for fetching main category ids

--- src/js/react/common/enum/LinkedEvents.ts
+++ src/js/react/common/enum/LinkedEvents.ts
@@ -4,7 +4,7 @@ const LinkedEvents = {
   MAIN_CATEGORY_URLS: {
     General: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:af44xv4uju/?include=keywords',
     Course: 'https://api.hel.fi/linkedevents/v1/keyword_set/helsinki:af44xwswri/?include=keywords',
-  },
+  } as Record<string, string>,
 };
 
 export default LinkedEvents;
